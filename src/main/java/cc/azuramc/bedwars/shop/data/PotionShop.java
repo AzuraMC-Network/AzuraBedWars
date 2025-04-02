28f74ae337547cb191c12d1b44829366ca0993a1
MindsMaster <1173348610@qq.com>
2025-04-02 11:27:57 +0800

    添加隐身药水

--- src/main/java/cc/azuramc/bedwars/shop/data/PotionShop.java
+++ src/main/java/cc/azuramc/bedwars/shop/data/PotionShop.java
@@ -13,6 +13,7 @@ import org.bukkit.potion.PotionEffectType;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 public class PotionShop implements ShopData {
@@ -22,9 +23,9 @@ public class PotionShop implements ShopData {
     public PotionShop() {
         mainShopItem = new ItemType(new ItemBuilderUtil().setType(MaterialUtil.BREWING_STAND()).setLores("§e点击查看！").getItem(), "§a药水", ColorType.NONE, null);
 
-        shopItems.add(new ItemType(new ItemBuilderUtil().setType(MaterialUtil.POTION()).setPotionData(new PotionEffect(PotionEffectType.SPEED, 600, 1)).setDisplayName("§b速度药水§7（30秒）").getItem(), "速度药水§7（30秒）", ColorType.NONE, new PriceCost(MaterialUtil.EMERALD(), 1, 100)));
-        shopItems.add(new ItemType(new ItemBuilderUtil().setType(MaterialUtil.POTION()).setPotionData(new PotionEffect(PotionEffectUtil.JUMP_BOOST(), 600, 1)).setDisplayName("跳跃药水§7（30秒）").getItem(), "跳跃药水§7（30秒）", ColorType.NONE, new PriceCost(MaterialUtil.EMERALD(), 1, 100)));
-        //shopItems.add(new ItemType(ItemUtil.makePotion(1, new PotionEffect(PotionEffectType.INVISIBILITY, 600, 1), "§b隐身药水§7（30秒）", null), "隐身药水§7（30秒）", ColorType.NONE, new PriceCost(MaterialUtil.EMERALD(), 2, 200)));
+        shopItems.add(new ItemType(new ItemBuilderUtil().setType(MaterialUtil.POTION()).setPotionData(new PotionEffect(PotionEffectType.SPEED, 600, 2)).setDisplayName("§b速度药水§7（30秒）").getItem(), "速度药水§7（30秒）", ColorType.NONE, new PriceCost(MaterialUtil.EMERALD(), 1, 100)));
+        shopItems.add(new ItemType(new ItemBuilderUtil().setType(MaterialUtil.POTION()).setPotionData(new PotionEffect(Objects.requireNonNull(PotionEffectUtil.JUMP_BOOST()), 600, 5)).setDisplayName("跳跃药水§7（30秒）").getItem(), "跳跃药水§7（30秒）", ColorType.NONE, new PriceCost(MaterialUtil.EMERALD(), 1, 100)));
+        shopItems.add(new ItemType(new ItemBuilderUtil().setType(MaterialUtil.POTION()).setPotionData(new PotionEffect(PotionEffectType.INVISIBILITY, 600, 1)).setDisplayName("§b隐身药水§7（30秒）").getItem(), "隐身药水§7（30秒）", ColorType.NONE, new PriceCost(MaterialUtil.EMERALD(), 2, 200)));
     }
 
 }
