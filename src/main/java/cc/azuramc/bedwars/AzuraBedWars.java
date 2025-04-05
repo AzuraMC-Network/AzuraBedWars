7c633c4f2e2c633690697c344e2d219525080a5e
MindsMaster <1173348610@qq.com>
2025-04-05 10:50:13 +0800

    优化主类代码

--- src/main/java/cc/azuramc/bedwars/AzuraBedWars.java
+++ src/main/java/cc/azuramc/bedwars/AzuraBedWars.java
@@ -23,21 +23,61 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * AzuraBedWars插件主类
+ * <p>
+ * 负责插件的初始化、配置加载、事件注册等核心功能
+ * </p>
+ */
 @Getter
 public final class AzuraBedWars extends JavaPlugin {
-    public static HashMap<Integer, Integer> playerLevel = new HashMap<>();
+    // 数据库相关常量
+    private static final String DB_PLAYER_DATA = "bwdata";
+    private static final String DB_PLAYER_STATS = "bwstats";
+    
+    // 配置相关常量
+    private static final String CONFIG_EDITOR_MODE = "editorMode";
+    private static final String CONFIG_DEFAULT_MAP = "defaultMapName";
+    private static final String CONFIG_MAP_STORAGE = "map.storage";
+    private static final String CONFIG_DB_MAP_NAME = "database.maps.name";
+    private static final String CONFIG_DB_MAP_TABLE = "database.maps.table";
+    
+    // 默认值常量
+    private static final String DEFAULT_STORAGE_TYPE = "JSON";
+    private static final String DEFAULT_DB_NAME = "bwdata";
+    private static final String DEFAULT_TABLE_NAME = "BWMaps";
+    
+    // 插件标识常量
+    private static final String PLUGIN_PREFIX = "[AzuraBedWars] ";
+
+    /** 玩家等级经验值映射表 */
+    private static final HashMap<Integer, Integer> playerLevel = new HashMap<>();
+    
+    /** 插件实例 */
     @Getter
     private static AzuraBedWars instance;
+    
+    /** 游戏实例 */
     @Getter
     private Game game;
+    
+    /** 地图管理器 */
     @Getter
     private MapManager mapManager;
+    
+    /** 当前地图数据 */
     @Getter
     private MapData mapData;
+    
+    /** 经济系统接口 */
     @Getter
     private Economy econ = null;
+    
+    /** 聊天系统接口 */
     @Getter
     private Chat chat = null;
+    
+    /** 数据库连接池管理器 */
     @Getter
     private ConnectionPoolHandler connectionPoolHandler;
 
@@ -45,76 +85,153 @@ public final class AzuraBedWars extends JavaPlugin {
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
-        long time = System.currentTimeMillis();
-        connectionPoolHandler = new ConnectionPoolHandler();
-
-        getConnectionPoolHandler().registerDatabase("bwdata");
-        getConnectionPoolHandler().registerDatabase("bwstats");
-
-        mapManager = new MapManager();
-        new CommandHandler(this);
-
-        if (!getConfig().getBoolean("editorMode")) {
-            new GUIListener(this);
-
-            setupEconomy();
-            setupChat();
-
-            initMapStorage();
-
-            mapManager.preloadAllMaps();
-            String defaultMapName = getConfig().getString("defaultMapName");
-            if (defaultMapName != null) {
-                mapData = mapManager.getAndLoadMapData(defaultMapName);
-            } else {
-                Map.Entry<String, MapData> firstEntry = mapManager.getLoadedMaps().entrySet().iterator().next();
-                mapData = firstEntry.getValue();
-                if (mapData == null) {
-                    Bukkit.getLogger().info("preloadMap 为空, 请先打开editorMode为服务端设置地图");
-                }
-            }
-
-            game = new Game(this);
-            game.loadGame(mapData);
-
-            Bukkit.getPluginManager().registerEvents(new JoinListener(), this);
-            Bukkit.getPluginManager().registerEvents(new QuitListener(), this);
-            Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
-            Bukkit.getPluginManager().registerEvents(new RespawnListener(), this);
-            Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
-            Bukkit.getPluginManager().registerEvents(new DamageListener(), this);
-            Bukkit.getPluginManager().registerEvents(new BlockListener(instance), this);
-            Bukkit.getPluginManager().registerEvents(new ServerListener(), this);
-            Bukkit.getPluginManager().registerEvents(new ChunkListener(), this);
-            Bukkit.getPluginManager().registerEvents(new AFKListener(), this);
-
-            Bukkit.getPluginManager().registerEvents(new LobbyBoard(game), this);
-            Bukkit.getPluginManager().registerEvents(new GameBoard(game), this);
-
-            SpecialItem.loadSpecials();
-            loadLevel();
-
-
-            Bukkit.getWorlds().forEach(world -> {
-                world.setAutoSave(false);
-                world.setDifficulty(Difficulty.NORMAL);
-            });
+        long startTime = System.currentTimeMillis();
+        
+        // 初始化基础服务
+        initDatabases();
+        initMapSystem();
+        initCommands();
+        
+        // 不在编辑模式下初始化游戏功能
+        if (!getConfig().getBoolean(CONFIG_EDITOR_MODE)) {
+            initGameFeatures();
         }
 
-        Bukkit.getConsoleSender().sendMessage("[AzuraBedWars] 加载完成耗时 " + (System.currentTimeMillis() - time) + " ms");
+        Bukkit.getConsoleSender().sendMessage(PLUGIN_PREFIX + "加载完成耗时 " + (System.currentTimeMillis() - startTime) + " ms");
+    }
+
+    /**
+     * 初始化数据库连接
+     */
+    private void initDatabases() {
+        connectionPoolHandler = new ConnectionPoolHandler();
+        connectionPoolHandler.registerDatabase(DB_PLAYER_DATA);
+        connectionPoolHandler.registerDatabase(DB_PLAYER_STATS);
+    }
+
+    /**
+     * 初始化地图系统
+     */
+    private void initMapSystem() {
+        mapManager = new MapManager();
+    }
+
+    /**
+     * 初始化命令处理器
+     */
+    private void initCommands() {
+        new CommandHandler(this);
+    }
+
+    /**
+     * 初始化游戏相关功能
+     */
+    private void initGameFeatures() {
+        // 注册GUI监听器
+        new GUIListener(this);
+
+        // 设置经济和聊天系统
+        setupEconomy();
+        setupChat();
+
+        // 初始化地图存储和加载默认地图
+        initMapStorage();
+        loadDefaultMap();
+
+        // 创建并加载游戏实例
+        game = new Game(this);
+        game.loadGame(mapData);
+
+        // 注册各种事件监听器
+        registerEventListeners();
+
+        // 加载特殊物品和玩家等级
+        SpecialItem.loadSpecials();
+        loadLevelData();
+
+        // 配置世界设置
+        configureWorlds();
+    }
+
+    /**
+     * 加载默认地图
+     */
+    private void loadDefaultMap() {
+        mapManager.preloadAllMaps();
+        String defaultMapName = getConfig().getString(CONFIG_DEFAULT_MAP);
+        
+        if (defaultMapName != null) {
+            mapData = mapManager.getAndLoadMapData(defaultMapName);
+        } else if (!mapManager.getLoadedMaps().isEmpty()) {
+            mapData = mapManager.getLoadedMaps().entrySet().iterator().next().getValue();
+        }
+        
+        if (mapData == null) {
+            Bukkit.getLogger().info("preloadMap 为空, 请先打开editorMode为服务端设置地图");
+        }
+    }
+
+    /**
+     * 注册所有事件监听器
+     */
+    private void registerEventListeners() {
+        // 玩家监听器
+        Bukkit.getPluginManager().registerEvents(new JoinListener(), this);
+        Bukkit.getPluginManager().registerEvents(new QuitListener(), this);
+        Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
+        Bukkit.getPluginManager().registerEvents(new RespawnListener(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+        Bukkit.getPluginManager().registerEvents(new DamageListener(), this);
+        Bukkit.getPluginManager().registerEvents(new AFKListener(), this);
+        
+        // 游戏世界监听器
+        Bukkit.getPluginManager().registerEvents(new BlockListener(instance), this);
+        Bukkit.getPluginManager().registerEvents(new ServerListener(), this);
+        Bukkit.getPluginManager().registerEvents(new ChunkListener(), this);
+        
+        // 记分板监听器
+        Bukkit.getPluginManager().registerEvents(new LobbyBoard(game), this);
+        Bukkit.getPluginManager().registerEvents(new GameBoard(game), this);
+    }
+
+    /**
+     * 配置游戏世界设置
+     */
+    private void configureWorlds() {
+        Bukkit.getWorlds().forEach(world -> {
+            world.setAutoSave(false);
+            world.setDifficulty(Difficulty.NORMAL);
+        });
     }
 
     @Override
     public void onDisable() {
-        connectionPoolHandler.closeAll();
-        game.getEventManager().stop();
+        if (connectionPoolHandler != null) {
+            connectionPoolHandler.closeAll();
+        }
+        
+        if (game != null && game.getEventManager() != null) {
+            game.getEventManager().stop();
+        }
     }
 
+    /**
+     * 在主线程上执行任务
+     * 
+     * @param runnable 要执行的任务
+     */
     public void mainThreadRunnable(Runnable runnable) {
         Bukkit.getScheduler().runTask(this, runnable);
     }
 
-    private void loadLevel() {
+    /**
+     * 加载玩家等级经验表
+     */
+    private void loadLevelData() {
+        // 清理先前数据
+        playerLevel.clear();
+        
+        // 等级1-10
         playerLevel.put(1, 0);
         playerLevel.put(2, 10);
         playerLevel.put(3, 25);
@@ -125,6 +242,8 @@ public final class AzuraBedWars extends JavaPlugin {
         playerLevel.put(8, 800);
         playerLevel.put(9, 900);
         playerLevel.put(10, 1050);
+        
+        // 等级11-20
         playerLevel.put(11, 1800);
         playerLevel.put(12, 2600);
         playerLevel.put(13, 3450);
@@ -135,6 +254,8 @@ public final class AzuraBedWars extends JavaPlugin {
         playerLevel.put(18, 7550);
         playerLevel.put(19, 8250);
         playerLevel.put(20, 8900);
+        
+        // 等级21-30
         playerLevel.put(21, 10000);
         playerLevel.put(22, 11250);
         playerLevel.put(23, 12500);
@@ -145,6 +266,8 @@ public final class AzuraBedWars extends JavaPlugin {
         playerLevel.put(28, 18750);
         playerLevel.put(29, 20000);
         playerLevel.put(30, 22000);
+        
+        // 等级31-40
         playerLevel.put(31, 24000);
         playerLevel.put(32, 26000);
         playerLevel.put(33, 28000);
@@ -155,6 +278,8 @@ public final class AzuraBedWars extends JavaPlugin {
         playerLevel.put(38, 38000);
         playerLevel.put(39, 40000);
         playerLevel.put(40, 45000);
+        
+        // 等级41-50
         playerLevel.put(41, 50000);
         playerLevel.put(42, 55000);
         playerLevel.put(43, 60000);
@@ -167,16 +292,29 @@ public final class AzuraBedWars extends JavaPlugin {
         playerLevel.put(50, 100000);
     }
 
-    public int getLevel(int level) {
-        int playerLevel = 0;
-        for (Map.Entry<Integer, Integer> entry : AzuraBedWars.playerLevel.entrySet()) {
-            if (level > entry.getValue()) {
-                playerLevel = entry.getKey();
-            } else break;
+    /**
+     * 根据经验值获取玩家等级
+     * 
+     * @param experience 玩家当前经验值
+     * @return 玩家等级
+     */
+    public int getLevel(int experience) {
+        int level = 1; // 默认最低等级为1
+        
+        for (Map.Entry<Integer, Integer> entry : playerLevel.entrySet()) {
+            if (experience >= entry.getValue()) {
+                level = entry.getKey();
+            } else {
+                break;
+            }
         }
-        return playerLevel;
+        
+        return level;
     }
 
+    /**
+     * 设置经济系统
+     */
     private void setupEconomy() {
         RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
         if (rsp != null) {
@@ -184,6 +322,9 @@ public final class AzuraBedWars extends JavaPlugin {
         }
     }
 
+    /**
+     * 设置聊天系统
+     */
     private void setupChat() {
         RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
         if (rsp != null) {
@@ -191,6 +332,11 @@ public final class AzuraBedWars extends JavaPlugin {
         }
     }
 
+    /**
+     * 调用Bukkit事件
+     * 
+     * @param event 要调用的事件
+     */
     public void callEvent(Event event) {
         getServer().getPluginManager().callEvent(event);
     }
@@ -199,29 +345,42 @@ public final class AzuraBedWars extends JavaPlugin {
      * 初始化地图存储系统
      */
     private void initMapStorage() {
-        // 加载默认配置到配置文件
-        if (!getConfig().contains("map.storage")) {
-            getConfig().set("map.storage", "JSON");
-            saveConfig();
-        }
-        
-        if (!getConfig().contains("database.maps.name")) {
-            getConfig().set("database.maps.name", "bwdata");
-            saveConfig();
-        }
-        
-        if (!getConfig().contains("database.maps.table")) {
-            getConfig().set("database.maps.table", "BWMaps");
-            saveConfig();
-        }
+        ensureConfigDefaults();
         
         // 确保数据库连接已注册
-        getConnectionPoolHandler().registerDatabase(getConfig().getString("database.maps.name", "bwdata"));
+        String dbName = getConfig().getString(CONFIG_DB_MAP_NAME, DEFAULT_DB_NAME);
+        connectionPoolHandler.registerDatabase(dbName);
         
         // 初始化默认存储
         MapStorageFactory.getDefaultStorage();
         
-        getLogger().info("地图存储系统已初始化，使用 " + getConfig().getString("map.storage", "JSON") + " 作为默认存储方式");
+        String storageType = getConfig().getString(CONFIG_MAP_STORAGE, DEFAULT_STORAGE_TYPE);
+        getLogger().info("地图存储系统已初始化，使用 " + storageType + " 作为默认存储方式");
     }
-
+    
+    /**
+     * 确保配置文件中包含必要的默认设置
+     */
+    private void ensureConfigDefaults() {
+        boolean configChanged = false;
+        
+        if (!getConfig().contains(CONFIG_MAP_STORAGE)) {
+            getConfig().set(CONFIG_MAP_STORAGE, DEFAULT_STORAGE_TYPE);
+            configChanged = true;
+        }
+        
+        if (!getConfig().contains(CONFIG_DB_MAP_NAME)) {
+            getConfig().set(CONFIG_DB_MAP_NAME, DEFAULT_DB_NAME);
+            configChanged = true;
+        }
+        
+        if (!getConfig().contains(CONFIG_DB_MAP_TABLE)) {
+            getConfig().set(CONFIG_DB_MAP_TABLE, DEFAULT_TABLE_NAME);
+            configChanged = true;
+        }
+        
+        if (configChanged) {
+            saveConfig();
+        }
+    }
 }
