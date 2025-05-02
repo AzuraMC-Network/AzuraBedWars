5c12d17ab76ff6d7b4f8ece2a6e47a7f5b422f79
MindsMaster <1173348610@qq.com>
2025-05-02 23:22:55 +0800

    代码实行阿里巴巴规范

--- src/main/java/cc/azuramc/bedwars/game/item/armor/ArmorType.java
+++ src/main/java/cc/azuramc/bedwars/game/item/armor/ArmorType.java
@@ -1,20 +1,16 @@
 package cc.azuramc.bedwars.game.item.armor;
 
+/**
+ * @author an5w1r@163.com
+ */
+
 public enum ArmorType {
-    /**
-     * 默认装备状态 （皮革）
-     */
+    /** 默认装备状态 （皮革） */
     DEFAULT,
-    /**
-     * 锁链套
-     */
+    /** 锁链套 */
     CHAINMAIL,
-    /**
-     * 铁套
-     */
+    /** 铁套 */
     IRON,
-    /**
-     * 钻石套
-     */
+    /** 钻石套 */
     DIAMOND
 }
--- src/main/java/cc/azuramc/bedwars/game/item/tool/ToolType.java
+++ src/main/java/cc/azuramc/bedwars/game/item/tool/ToolType.java
@@ -1,24 +1,18 @@
 package cc.azuramc.bedwars.game.item.tool;
 
+/**
+ * @author an5w1r@163.com
+ */
+
 public enum ToolType {
-    /**
-     * 无工具
-     */
+    /** 无工具 */
     NONE,
-    /**
-     * 木制工具
-     */
+    /** 木制工具 */
     WOOD,
-    /**
-     * 石制工具
-     */
+    /** 石制工具 */
     STONE,
-    /**
-     * 铁制工具
-     */
+    /** 铁制工具 */
     IRON,
-    /**
-     * 钻石制工具
-     */
+    /** 钻石制工具*/
     DIAMOND
 }
