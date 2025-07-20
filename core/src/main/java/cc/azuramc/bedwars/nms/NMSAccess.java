bbfe32902633479445c13eb96d8fa02a73a05bc7
MindsMaster <1173348610@qq.com>
2025-07-21 01:08:08 +0800

    修复火焰弹发射不准的问题 #26

--- core/src/main/java/cc/azuramc/bedwars/nms/NMSAccess.java
+++ core/src/main/java/cc/azuramc/bedwars/nms/NMSAccess.java
@@ -1,7 +1,9 @@
 package cc.azuramc.bedwars.nms;
 
 import cc.azuramc.bedwars.game.GamePlayer;
+import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 import java.util.List;
 
@@ -13,4 +15,5 @@ public interface NMSAccess {
     void showArmor(Player victim, Player receiver);
     void hideArmor(GamePlayer gamePlayer, List<GamePlayer> receiverList);
     void showArmor(GamePlayer gamePlayer, List<GamePlayer> receiverList);
+    Fireball setFireballDirection(Fireball fireball, Vector vector);
 }
