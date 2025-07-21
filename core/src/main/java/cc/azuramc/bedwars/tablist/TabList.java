c14588bc421f70d88f3580e4d2fbafc7bde4d815
MindsMaster <1173348610@qq.com>
2025-07-21 17:40:16 +0800

    添加设置玩家nametag的功能 #28

--- core/src/main/java/cc/azuramc/bedwars/tablist/TabList.java
+++ core/src/main/java/cc/azuramc/bedwars/tablist/TabList.java
@@ -3,7 +3,11 @@ package cc.azuramc.bedwars.tablist;
 import cc.azuramc.bedwars.game.GamePlayer;
 import cc.azuramc.bedwars.game.GameTeam;
 import cc.azuramc.bedwars.util.LuckPermsUtil;
+import cc.azuramc.bedwars.util.MessageUtil;
 import cc.azuramc.bedwars.util.VaultUtil;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 
 /**
  * @author an5w1r@163.com
@@ -29,6 +33,8 @@ public class TabList {
         
         if (gameTeam != null) {
             displayName = gameTeam.getChatColor() + gameTeam.getName() + " | " + gamePlayer.getNickName();
+
+            setPlayerNameTag(gamePlayer, gameTeam.getChatColor().toString(), gameTeam.getChatColor().toString());
         } else {
             // 如果玩家没有队伍，只显示玩家名
             displayName = "§7" + gamePlayer.getNickName();
@@ -92,5 +98,32 @@ public class TabList {
         if (gamePlayer != null) {
             gamePlayer.getPlayer().setPlayerListName(gamePlayer.getName());
         }
+    }
+
+    /**
+     * 使用ScoreboardAPI设置玩家tag
+     * @param gamePlayer 游戏玩家
+     * @param prefix 前缀
+     * @param suffix 后缀
+     */
+    public static void setPlayerNameTag(GamePlayer gamePlayer, String prefix, String suffix) {
+        Player player = gamePlayer.getPlayer();
+        Scoreboard scoreboard = player.getScoreboard();
+
+        // 检查是否已有队伍
+        String teamName = "tag_" + player.getName();
+        Team team = scoreboard.getTeam(teamName);
+        if (team == null) {
+            team = scoreboard.registerNewTeam(teamName);
+        }
+
+        // 设置前缀和后缀
+        team.setPrefix(MessageUtil.color(prefix));
+        team.setSuffix(MessageUtil.color(suffix));
+
+        // 添加玩家到这个队伍
+        team.addEntry(player.getName());
+
+        player.setScoreboard(scoreboard);
     }
 }
