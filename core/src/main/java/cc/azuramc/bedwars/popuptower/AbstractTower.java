c6e79422fc84ad9bb47b03a99480714b4c943f4e
MindsMaster <1173348610@qq.com>
2025-07-22 13:46:56 +0800

    popup tower optimized code

---
+++ core/src/main/java/cc/azuramc/bedwars/popuptower/AbstractTower.java
@@ -0,0 +1,99 @@
+package cc.azuramc.bedwars.popuptower;
+
+import cc.azuramc.bedwars.AzuraBedWars;
+import cc.azuramc.bedwars.compat.util.PlayerUtil;
+import cc.azuramc.bedwars.game.GamePlayer;
+import cc.azuramc.bedwars.game.TeamColor;
+import com.cryptomorin.xseries.XSound;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.List;
+
+/**
+ * @author An5w1r@163.com
+ */
+public abstract class AbstractTower {
+    private static final int BLOCKS_PER_TICK = 2;
+    private static final long TICK_DELAY = 1L;
+
+    private BukkitTask buildTask;
+
+    public AbstractTower(Location location, Block chest, TeamColor color, Player player) {
+        consumeItemFromHand(player);
+        List<String> coordinates = getTowerCoordinates();
+        startBuilding(coordinates, chest, color, player);
+    }
+
+    /**
+     * 子类提供具体的坐标数据
+     */
+    protected abstract List<String> getTowerCoordinates();
+
+    /**
+     * 消耗玩家手中的物品
+     */
+    private void consumeItemFromHand(Player player) {
+        ItemStack itemInHand = PlayerUtil.getItemInHand(player);
+        if (itemInHand == null) return;
+
+        if (itemInHand.getAmount() > 1) {
+            itemInHand.setAmount(itemInHand.getAmount() - 1);
+        } else {
+            PlayerUtil.setItemInHand(player, null);
+        }
+    }
+
+    /**
+     * 开始建造塔
+     */
+    private void startBuilding(List<String> coordinates, Block chest, TeamColor color, Player player) {
+        final int[] currentIndex = {0};
+        final int totalBlocks = coordinates.size();
+
+        this.buildTask = Bukkit.getScheduler().runTaskTimer(
+                AzuraBedWars.getInstance(),
+                () -> {
+                    GamePlayer.get(player).playSound(XSound.ENTITY_CHICKEN_EGG.get(), 1.0F, 0.5F);
+
+                    for (int i = 0; i < BLOCKS_PER_TICK && currentIndex[0] < totalBlocks; i++) {
+                        String coordinate = coordinates.get(currentIndex[0]);
+                        placeBlock(coordinate, chest, color, player);
+                        currentIndex[0]++;
+                    }
+
+                    if (currentIndex[0] >= totalBlocks) {
+                        this.buildTask.cancel();
+                    }
+                },
+                0L,
+                TICK_DELAY
+        );
+    }
+
+    /**
+     * 放置单个方块
+     */
+    private void placeBlock(String coordinate, Block chest, TeamColor color, Player player) {
+        if (coordinate.contains("ladder")) {
+            String[] parts = coordinate.split("ladder");
+            int ladderData = Integer.parseInt(parts[1]);
+            new NewPlaceBlock(chest, coordinate, color, player, true, ladderData);
+        } else {
+            new NewPlaceBlock(chest, coordinate, color, player, false, 0);
+        }
+    }
+
+    /**
+     * 取消建造任务
+     */
+    public void cancelBuilding() {
+        if (buildTask != null && !buildTask.isCancelled()) {
+            buildTask.cancel();
+        }
+    }
+}
--- core/src/main/java/cc/azuramc/bedwars/popuptower/TowerEast.java
+++ core/src/main/java/cc/azuramc/bedwars/popuptower/TowerEast.java
@@ -1,169 +1,65 @@
 package cc.azuramc.bedwars.popuptower;
 
-import cc.azuramc.bedwars.AzuraBedWars;
-import cc.azuramc.bedwars.compat.util.PlayerUtil;
-import cc.azuramc.bedwars.game.GamePlayer;
 import cc.azuramc.bedwars.game.TeamColor;
-import com.cryptomorin.xseries.XSound;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitTask;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class TowerEast {
-    private BukkitTask task;
-    public TowerEast(Location loc, Block chest, TeamColor color, Player player) {
-        ItemStack itemInHand = PlayerUtil.getItemInHand(player);
-        if (itemInHand.getAmount() > 1) {
-            itemInHand.setAmount(itemInHand.getAmount() - 1);
-        } else {
-            PlayerUtil.setItemInHand(player, null);
-        }
+/**
+ * @author An5w1r@163.com
+ */
+public class TowerEast extends AbstractTower {
 
-        List<String> relloc = new ArrayList<>();
-        relloc.add("2, 0, -1");
-        relloc.add("1, 0, -2");
-        relloc.add("0, 0, -2");
-        relloc.add("-1, 0, -1");
-        relloc.add("-1, 0, 0");
-        relloc.add("-1, 0, 1");
-        relloc.add("0, 0, 2");
-        relloc.add("1, 0, 2");
-        relloc.add("2, 0, 1");
-        relloc.add("0, 0, 0, ladder5");
-        relloc.add("2, 1, -1");
-        relloc.add("1, 1, -2");
-        relloc.add("0, 1, -2");
-        relloc.add("-1, 1, -1");
-        relloc.add("-1, 1, 0");
-        relloc.add("-1, 1, 1");
-        relloc.add("0, 1, 2");
-        relloc.add("1, 1, 2");
-        relloc.add("2, 1, 1");
-        relloc.add("0, 1, 0, ladder5");
-        relloc.add("2, 2, -1");
-        relloc.add("1, 2, -2");
-        relloc.add("0, 2, -2");
-        relloc.add("-1, 2, -1");
-        relloc.add("-1, 2, 0");
-        relloc.add("-1, 2, 1");
-        relloc.add("0, 2, 2");
-        relloc.add("1, 2, 2");
-        relloc.add("2, 2, 1");
-        relloc.add("0, 2, 0, ladder5");
-        relloc.add("2, 3, 0");
-        relloc.add("2, 3, -1");
-        relloc.add("1, 3, -2");
-        relloc.add("0, 3, -2");
-        relloc.add("-1, 3, -1");
-        relloc.add("-1, 3, 0");
-        relloc.add("-1, 3, 1");
-        relloc.add("0, 3, 2");
-        relloc.add("1, 3, 2");
-        relloc.add("2, 3, 1");
-        relloc.add("0, 3, 0, ladder5");
-        relloc.add("2, 4, 0");
-        relloc.add("2, 4, -1");
-        relloc.add("1, 4, -2");
-        relloc.add("0, 4, -2");
-        relloc.add("-1, 4, -1");
-        relloc.add("-1, 4, 0");
-        relloc.add("-1, 4, 1");
-        relloc.add("0, 4, 2");
-        relloc.add("1, 4, 2");
-        relloc.add("2, 4, 1");
-        relloc.add("0, 4, 0, ladder5");
-        relloc.add("-1, 5, -2");
-        relloc.add("0, 5, -2");
-        relloc.add("1, 5, -2");
-        relloc.add("2, 5, -2");
-        relloc.add("-1, 5, -1");
-        relloc.add("0, 5, -1");
-        relloc.add("1, 5, -1");
-        relloc.add("2, 5, -1");
-        relloc.add("-1, 5, 0");
-        relloc.add("1, 5, 0");
-        relloc.add("2, 5, 0");
-        relloc.add("-1, 5, 1");
-        relloc.add("0, 5, 0, ladder5");
-        relloc.add("0, 5, 1");
-        relloc.add("1, 5, 1");
-        relloc.add("2, 5, 1");
-        relloc.add("-1, 5, 2");
-        relloc.add("0, 5, 2");
-        relloc.add("1, 5, 2");
-        relloc.add("2, 5, 2");
-        relloc.add("2, 5, -3");
-        relloc.add("2, 6, -3");
-        relloc.add("2, 7, -3");
-        relloc.add("1, 6, -3");
-        relloc.add("0, 6, -3");
-        relloc.add("-1, 5, -3");
-        relloc.add("-1, 6, -3");
-        relloc.add("-1, 7, -3");
-        relloc.add("-2, 5, -2");
-        relloc.add("-2, 6, -2");
-        relloc.add("-2, 7, -2");
-        relloc.add("-2, 6, -1");
-        relloc.add("-2, 5, 0");
-        relloc.add("-2, 6, 0");
-        relloc.add("-2, 7, 0");
-        relloc.add("-2, 6, 1");
-        relloc.add("-2, 5, 2");
-        relloc.add("-2, 6, 2");
-        relloc.add("-2, 7, 2");
-        relloc.add("2, 5, 3");
-        relloc.add("2, 6, 3");
-        relloc.add("2, 7, 3");
-        relloc.add("1, 6, 3");
-        relloc.add("0, 6, 3");
-        relloc.add("-1, 5, 3");
-        relloc.add("-1, 6, 3");
-        relloc.add("-1, 7, 3");
-        relloc.add("3, 5, -2");
-        relloc.add("3, 6, -2");
-        relloc.add("3, 7, -2");
-        relloc.add("3, 6, -1");
-        relloc.add("3, 5, 0");
-        relloc.add("3, 6, 0");
-        relloc.add("3, 7, 0");
-        relloc.add("3, 6, 1");
-        relloc.add("3, 5, 2");
-        relloc.add("3, 6, 2");
-        relloc.add("3, 7, 2");
-        int[] i = new int[]{0};
-        this.task = Bukkit.getScheduler().runTaskTimer(AzuraBedWars.getInstance(), () -> {
-            GamePlayer.get(player).playSound(XSound.ENTITY_CHICKEN_EGG.get(), 1.0F, 0.5F);
-            if (relloc.size() + 1 == i[0] + 1) {
-                this.task.cancel();
-            } else {
-                String c1 = relloc.get(i[0]);
-                if (c1.contains("ladder")) {
-                    int ldata = Integer.parseInt(c1.split("ladder")[1]);
-                    new NewPlaceBlock(chest, c1, color, player, true, ldata);
-                } else {
-                    new NewPlaceBlock(chest, c1, color, player, false, 0);
-                }
+    public TowerEast(Location location, Block chest, TeamColor color, Player player) {
+        super(location, chest, color, player);
+    }
 
-                if (relloc.size() + 1 == i[0] + 2) {
-                    this.task.cancel();
-                } else {
-                    String c2 = relloc.get(i[0] + 1);
-                    if (c2.contains("ladder")) {
-                        int ldatax = Integer.parseInt(c2.split("ladder")[1]);
-                        new NewPlaceBlock(chest, c2, color, player, true, ldatax);
-                    } else {
-                        new NewPlaceBlock(chest, c2, color, player, false, 0);
-                    }
+    @Override
+    protected List<String> getTowerCoordinates() {
+        return Arrays.asList(
+                // 第0层 - 地基
+                "2, 0, -1", "1, 0, -2", "0, 0, -2", "-1, 0, -1", "-1, 0, 0",
+                "-1, 0, 1", "0, 0, 2", "1, 0, 2", "2, 0, 1", "0, 0, 0, ladder5",
 
-                    i[0] += 2;
-                }
-            }
-        }, 0L, 1L);
+                // 第1层
+                "2, 1, -1", "1, 1, -2", "0, 1, -2", "-1, 1, -1", "-1, 1, 0",
+                "-1, 1, 1", "0, 1, 2", "1, 1, 2", "2, 1, 1", "0, 1, 0, ladder5",
+
+                // 第2层
+                "2, 2, -1", "1, 2, -2", "0, 2, -2", "-1, 2, -1", "-1, 2, 0",
+                "-1, 2, 1", "0, 2, 2", "1, 2, 2", "2, 2, 1", "0, 2, 0, ladder5",
+
+                // 第3层
+                "2, 3, 0", "2, 3, -1", "1, 3, -2", "0, 3, -2", "-1, 3, -1",
+                "-1, 3, 0", "-1, 3, 1", "0, 3, 2", "1, 3, 2", "2, 3, 1", "0, 3, 0, ladder5",
+
+                // 第4层
+                "2, 4, 0", "2, 4, -1", "1, 4, -2", "0, 4, -2", "-1, 4, -1",
+                "-1, 4, 0", "-1, 4, 1", "0, 4, 2", "1, 4, 2", "2, 4, 1", "0, 4, 0, ladder5",
+
+                // 第5层 - 平台
+                "-1, 5, -2", "0, 5, -2", "1, 5, -2", "2, 5, -2",
+                "-1, 5, -1", "0, 5, -1", "1, 5, -1", "2, 5, -1",
+                "-1, 5, 0", "1, 5, 0", "2, 5, 0",
+                "-1, 5, 1", "0, 5, 0, ladder5", "0, 5, 1", "1, 5, 1", "2, 5, 1",
+                "-1, 5, 2", "0, 5, 2", "1, 5, 2", "2, 5, 2",
+
+                // 围墙支柱
+                "2, 5, -3", "2, 6, -3", "2, 7, -3",
+                "1, 6, -3", "0, 6, -3",
+                "-1, 5, -3", "-1, 6, -3", "-1, 7, -3",
+                "-2, 5, -2", "-2, 6, -2", "-2, 7, -2", "-2, 6, -1",
+                "-2, 5, 0", "-2, 6, 0", "-2, 7, 0", "-2, 6, 1",
+                "-2, 5, 2", "-2, 6, 2", "-2, 7, 2",
+                "2, 5, 3", "2, 6, 3", "2, 7, 3",
+                "1, 6, 3", "0, 6, 3",
+                "-1, 5, 3", "-1, 6, 3", "-1, 7, 3",
+                "3, 5, -2", "3, 6, -2", "3, 7, -2", "3, 6, -1",
+                "3, 5, 0", "3, 6, 0", "3, 7, 0", "3, 6, 1",
+                "3, 5, 2", "3, 6, 2", "3, 7, 2"
+        );
     }
 }
