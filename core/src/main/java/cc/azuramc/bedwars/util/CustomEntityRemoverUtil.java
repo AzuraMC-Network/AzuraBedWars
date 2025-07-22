be3a4fa3d53004d52f1865c1e430db6f691b7d68
Marina-HCL <2511779002@qq.com>
2025-07-22 15:58:15 +0800

    完善铁傀儡和蠹虫的生成名称 #34
    Former-commit-id: 5d587b7ea40cfda9ee5cdb3c77e32021afb6d714

--- core/src/main/java/cc/azuramc/bedwars/util/CustomEntityRemoverUtil.java
+++ core/src/main/java/cc/azuramc/bedwars/util/CustomEntityRemoverUtil.java
@@ -2,14 +2,17 @@ package cc.azuramc.bedwars.util;
 
 import cc.azuramc.bedwars.game.GameTeam;
 import lombok.Getter;
+import org.bukkit.entity.IronGolem;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Silverfish;
 
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class CustomEntityRemoverUtil {
 
-    @Getter private static final ConcurrentHashMap<UUID, CustomEntityRemoverUtil> despawnables = new ConcurrentHashMap<>();
+    @Getter
+    private static final ConcurrentHashMap<UUID, CustomEntityRemoverUtil> despawnables = new ConcurrentHashMap<>();
 
     @Getter
     private LivingEntity livingEntity;
@@ -34,7 +37,7 @@ public class CustomEntityRemoverUtil {
     public void refresh() {
         if (livingEntity.isDead() || livingEntity == null || gameTeam == null) {
             getDespawnables().remove(uuid);
-            if (gameTeam == null){
+            if (gameTeam == null) {
                 if (livingEntity != null) {
                     livingEntity.damage(livingEntity.getHealth() + 100);
                 }
@@ -44,21 +47,26 @@ public class CustomEntityRemoverUtil {
         setName();
         despawn--;
         if (despawn == 0) {
-            livingEntity.damage(livingEntity.getHealth()+100);
+            livingEntity.damage(livingEntity.getHealth() + 100);
             getDespawnables().remove(livingEntity.getUniqueId());
         }
     }
 
     private void setName() {
-        String name = "test";
+        String name = "";
+        if (livingEntity instanceof IronGolem) {
+            name = "{TeamColor}{TeamName}队的守卫";
+        } else if (livingEntity instanceof Silverfish) {
+            name = "{TeamColor}{TeamName}队的蠹虫";
+        }
         if (gameTeam != null) {
             name = name.replace("{TeamColor}", gameTeam.getChatColor().toString()).replace("{TeamName}", gameTeam.getName());
         }
         livingEntity.setCustomName(name);
     }
 
-    public void destroy(){
-        if (getLivingEntity() != null){
+    public void destroy() {
+        if (getLivingEntity() != null) {
             getLivingEntity().damage(Integer.MAX_VALUE);
         }
         gameTeam = null;
