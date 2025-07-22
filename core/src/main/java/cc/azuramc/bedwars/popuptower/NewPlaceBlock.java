079bed4f80a4891ed0fd3ca22a517f48d56a8651
MindsMaster <1173348610@qq.com>
2025-07-22 14:49:41 +0800

    修复梯子放置方向不正确问题 #34
    Former-commit-id: 5ef33d0abedf691de56cffdf5e9c871fa6973f7d

--- core/src/main/java/cc/azuramc/bedwars/popuptower/NewPlaceBlock.java
+++ core/src/main/java/cc/azuramc/bedwars/popuptower/NewPlaceBlock.java
@@ -1,7 +1,7 @@
 package cc.azuramc.bedwars.popuptower;
 
+import cc.azuramc.bedwars.AzuraBedWars;
 import cc.azuramc.bedwars.game.TeamColor;
-import com.cryptomorin.xseries.XBlock;
 import com.cryptomorin.xseries.XMaterial;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -9,27 +9,22 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 
 public class NewPlaceBlock {
-    public NewPlaceBlock(Block b, String xyz, TeamColor color, Player p, boolean ladder, int ladderdata) {
+    public NewPlaceBlock(Block block, String xyz, TeamColor color, Player p, boolean ladder, int ladderdata) {
         int x = Integer.parseInt(xyz.split(", ")[0]);
         int y = Integer.parseInt(xyz.split(", ")[1]);
         int z = Integer.parseInt(xyz.split(", ")[2]);
-        if (b.getRelative(x, y, z).getType().equals(Material.AIR)) {
+        if (block.getRelative(x, y, z).getType().equals(Material.AIR)) {
 
             if (!ladder)
-                placeTowerBlocks(b, color, x, y, z);
+                placeTowerBlocks(block, color, x, y, z);
             else
-                placeLadder(b, x, y, z, ladderdata);
+                AzuraBedWars.getInstance().getNmsAccess().placeLadder(block, x, y, z, ladderdata);
         }
 
     }
 
     private void placeTowerBlocks(org.bukkit.block.Block block, TeamColor color, int x, int y, int z){
         block.getRelative(x, y, z).setType(XMaterial.WHITE_WOOL.get());
-    }
-
-    private void placeLadder(org.bukkit.block.Block block, int x, int y, int z, int ladderdata){
-        block.getRelative(x, y, z).setType(Material.LADDER);
-        XBlock.setDirection(block, getFaceByData(ladderdata));
     }
 
     private BlockFace getFaceByData(int data) {
