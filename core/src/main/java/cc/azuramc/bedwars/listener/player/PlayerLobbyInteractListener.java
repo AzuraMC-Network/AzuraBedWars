15db9c7c03eacbd20cc2a60a3377347f143fe7c8
MindsMaster <1173348610@qq.com>
2025-07-23 13:03:28 +0800

    更换为使用if else,以便适配XMaterial #38

--- core/src/main/java/cc/azuramc/bedwars/listener/player/PlayerLobbyInteractListener.java
+++ core/src/main/java/cc/azuramc/bedwars/listener/player/PlayerLobbyInteractListener.java
@@ -7,6 +7,7 @@ import cc.azuramc.bedwars.game.GameState;
 import cc.azuramc.bedwars.gui.ModeSelectionGUI;
 import cc.azuramc.bedwars.gui.TeamSelectionGUI;
 import cc.azuramc.bedwars.util.BungeeUtil;
+import com.cryptomorin.xseries.XMaterial;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -38,18 +39,12 @@ public class PlayerLobbyInteractListener implements Listener {
     private void handleWaitingState(PlayerInteractEvent event, GamePlayer gamePlayer, Material interactingMaterial) {
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) {
             event.setCancelled(true);
-            switch (interactingMaterial) {
-                case PAPER:
-                    new ModeSelectionGUI(gamePlayer).open();
-                    break;
-                case SLIME_BALL:
-                    BungeeUtil.connectToLobby(gamePlayer);
-                    break;
-                case RED_WOOL, YELLOW_WOOL, BLUE_WOOL, GREEN_WOOL, WHITE_WOOL:
-                    new TeamSelectionGUI(gamePlayer).open();
-                    break;
-                default:
-                    break;
+            if (interactingMaterial == XMaterial.PAPER.get()) {
+                new ModeSelectionGUI(gamePlayer).open();
+            } else if (interactingMaterial == XMaterial.SLIME_BALL.get()) {
+                BungeeUtil.connectToLobby(gamePlayer);
+            } else if (interactingMaterial.name().contains("WOOL")) {
+                new TeamSelectionGUI(gamePlayer).open();
             }
         }
     }
