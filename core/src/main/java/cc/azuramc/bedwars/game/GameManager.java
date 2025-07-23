51e530af8dafd33154126ae96bc4c43054af7c2f
MindsMaster <1173348610@qq.com>
2025-07-23 13:16:29 +0800

    提取出羊毛颜色相关工具类

--- core/src/main/java/cc/azuramc/bedwars/game/GameManager.java
+++ core/src/main/java/cc/azuramc/bedwars/game/GameManager.java
@@ -2,9 +2,9 @@ package cc.azuramc.bedwars.game;
 
 import cc.azuramc.bedwars.AzuraBedWars;
 import cc.azuramc.bedwars.api.event.BedwarsGameStartEvent;
-import cc.azuramc.bedwars.compat.VersionUtil;
 import cc.azuramc.bedwars.compat.util.ItemBuilder;
 import cc.azuramc.bedwars.compat.util.PlayerUtil;
+import cc.azuramc.bedwars.compat.util.WoolUtil;
 import cc.azuramc.bedwars.config.object.ItemConfig;
 import cc.azuramc.bedwars.config.object.MessageConfig;
 import cc.azuramc.bedwars.config.object.SettingsConfig;
@@ -251,7 +251,7 @@ public class GameManager {
                     if (block.getType().name().contains("WOOL")) {
                         // 根据羊毛颜色确定队伍颜色
                         LoggerUtil.debug("GameManager$detectTeamColorFromWool | Found WOOL block! Type: " + block.getType().name());
-                        return getTeamColorFromWoolBlock(block);
+                        return WoolUtil.getTeamColorFromWoolBlock(block);
                     }
                 }
             }
@@ -259,95 +259,6 @@ public class GameManager {
 
         LoggerUtil.debug("GameManager$detectTeamColorFromWool | Checked " + checkedBlocks + " blocks, no wool found");
         return null;
-    }
-
-    /**
-     * 从羊毛方块确定对应的队伍颜色
-     *
-     * @param block 羊毛方块
-     * @return 对应的队伍颜色
-     */
-    private TeamColor getTeamColorFromWoolBlock(Block block) {
-        String blockType = block.getType().name();
-
-        if (!VersionUtil.isLessThan113()) {
-            // 1.13+版本的羊毛命名格式为 COLOR_WOOL
-            String colorName = blockType.substring(0, blockType.length() - 5);
-            // 映射颜色名称到TeamColor
-            return switch (colorName) {
-                case "RED" -> TeamColor.RED;
-                case "BLUE" -> TeamColor.BLUE;
-                case "GREEN" -> TeamColor.GREEN;
-                case "LIME" -> TeamColor.LIME;
-                case "YELLOW" -> TeamColor.YELLOW;
-                case "CYAN" -> TeamColor.CYAN;
-                case "LIGHT_BLUE" -> TeamColor.LIGHT_BLUE;
-                case "WHITE" -> TeamColor.WHITE;
-                case "PINK" -> TeamColor.PINK;
-                case "MAGENTA" -> TeamColor.MAGENTA;
-                case "PURPLE" -> TeamColor.PURPLE;
-                case "GRAY" -> TeamColor.GRAY;
-                case "BLACK" -> TeamColor.BLACK;
-                case "ORANGE" -> TeamColor.ORANGE;
-                case "BROWN" -> TeamColor.BROWN;
-                default -> null;
-            };
-        } else if ("WOOL".equals(blockType)) {
-            // 1.12-版本需要检查数据值
-            @SuppressWarnings("deprecation")
-            byte data = block.getData();
-
-            // 根据羊毛的数据值映射到TeamColor
-            return switch (data) {
-                case 0 -> TeamColor.WHITE;
-                case 1 -> TeamColor.ORANGE;
-                case 2 -> TeamColor.MAGENTA;
-                case 3 -> TeamColor.LIGHT_BLUE;
-                case 4 -> TeamColor.YELLOW;
-                case 5 -> TeamColor.LIME;
-                case 6 -> TeamColor.PINK;
-                case 7 -> TeamColor.GRAY;
-                case 9 -> TeamColor.CYAN;
-                case 10 -> TeamColor.PURPLE;
-                case 11 -> TeamColor.BLUE;
-                case 12 -> TeamColor.BROWN;
-                case 13 -> TeamColor.GREEN;
-                case 14 -> TeamColor.RED;
-                case 15 -> TeamColor.BLACK;
-                default -> null;
-            };
-        }
-
-        return null;
-    }
-
-    /**
-     * 低版本根据TeamColor获取BlockData
-     *
-     * @param teamColor 队伍颜色
-     * @return Block Data 的 byte数值
-     */
-    public byte getWoolDataFromTeamColor(TeamColor teamColor) {
-
-        // 根据羊毛的数据值映射到TeamColor
-        return switch (teamColor) {
-            case WHITE -> 0;
-            case ORANGE -> 1;
-            case MAGENTA -> 2;
-            case LIGHT_BLUE -> 3;
-            case YELLOW -> 4;
-            case LIME -> 5;
-            case PINK -> 6;
-            case GRAY -> 7;
-            case CYAN -> 9;
-            case PURPLE -> 10;
-            case BLUE -> 11;
-            case BROWN -> 12;
-            case GREEN -> 13;
-            case RED -> 14;
-            case BLACK -> 15;
-            default -> 0;
-        };
     }
 
     /**
