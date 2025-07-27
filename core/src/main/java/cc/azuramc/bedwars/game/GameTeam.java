72f0142e9b850f7ca3c95c5a105529c5a71537ce
MindsMaster <1173348610@qq.com>
2025-07-27 23:05:11 +0800

    添加团队获取资源掉落点的方法 #49
    Former-commit-id: c9191e3697a2f54cd78fe56863bda63c1c097c38

--- core/src/main/java/cc/azuramc/bedwars/game/GameTeam.java
+++ core/src/main/java/cc/azuramc/bedwars/game/GameTeam.java
@@ -29,6 +29,7 @@ public class GameTeam {
 
     private final TeamColor teamColor;
     private final Location spawnLocation;
+    private final Location resourceDropLocation;
     private int maxPlayers;
 
     private Block bedFeet;
@@ -54,18 +55,19 @@ public class GameTeam {
      * @param location 出生点位置
      * @param maxPlayers 最大玩家数
      */
-    public GameTeam(TeamColor teamColor, Location location, int maxPlayers) {
+    public GameTeam(TeamColor teamColor, Location location, Location resourceDropLocation, int maxPlayers) {
         this.teamColor = Objects.requireNonNull(teamColor, "团队颜色不能为空");
         this.spawnLocation = Objects.requireNonNull(location, "出生点位置不能为空");
+        this.resourceDropLocation = Objects.requireNonNull(resourceDropLocation, "资源掉落位置不能为空");
         this.maxPlayers = maxPlayers;
-        
+
         // 初始化默认值
         initializeDefaults();
-        
+
         // 初始化床相关字段
         new GameTeamBedHandler(this);
     }
-    
+
     /**
      * 初始化团队默认属性值
      */
@@ -73,7 +75,7 @@ public class GameTeam {
         // 床状态初始化
         this.hasBed = false;
         this.isDestroyed = false;
-        
+
         // 团队升级初始化
         this.hasSharpnessUpgrade = false;
         this.protectionUpgrade = 0;
@@ -87,7 +89,7 @@ public class GameTeam {
 
     /**
      * 获取团队聊天颜色
-     * 
+     *
      * @return 团队对应的聊天颜色
      */
     public ChatColor getChatColor() {
@@ -96,7 +98,7 @@ public class GameTeam {
 
     /**
      * 获取团队染料颜色
-     * 
+     *
      * @return 团队对应的染料颜色
      */
     public DyeColor getDyeColor() {
@@ -105,7 +107,7 @@ public class GameTeam {
 
     /**
      * 获取团队颜色对象
-     * 
+     *
      * @return 团队对应的颜色对象
      */
     public Color getColor() {
@@ -114,7 +116,7 @@ public class GameTeam {
 
     /**
      * 获取团队名称
-     * 
+     *
      * @return 团队名称
      */
     public String getName() {
@@ -123,12 +125,12 @@ public class GameTeam {
 
     /**
      * 获取团队中所有玩家
-     * 
+     *
      * @return 团队玩家列表
      */
     public List<GamePlayer> getGamePlayers() {
         List<GamePlayer> teamPlayers = new ArrayList<>();
-        
+
         for (GamePlayer gamePlayer : GamePlayer.getGamePlayers()) {
             if (gamePlayer.getGameTeam() == this) {
                 teamPlayers.add(gamePlayer);
@@ -140,24 +142,24 @@ public class GameTeam {
 
     /**
      * 获取团队中所有存活的玩家
-     * 
+     *
      * @return 存活玩家列表
      */
     public List<GamePlayer> getAlivePlayers() {
         List<GamePlayer> alivePlayers = new ArrayList<>();
-        
+
         for (GamePlayer gamePlayer : getGamePlayers()) {
             if (gamePlayer.isOnline() && !gamePlayer.isSpectator()) {
                 alivePlayers.add(gamePlayer);
             }
         }
-        
+
         return alivePlayers;
     }
 
     /**
      * 检查指定玩家是否在团队中
-     * 
+     *
      * @param gamePlayer 要检查的玩家
      * @return 如果玩家在团队中返回true，否则返回false
      */
@@ -165,13 +167,13 @@ public class GameTeam {
         if (gamePlayer == null) {
             return false;
         }
-        
+
         return gamePlayer.getGameTeam() == this;
     }
 
     /**
      * 检查指定玩家是否在团队中，排除特定玩家
-     * 
+     *
      * @param excludedPlayer 要排除的玩家
      * @param targetPlayer 要检查的玩家
      * @return 如果玩家在团队中且不是排除的玩家，返回true，否则返回false
@@ -180,13 +182,13 @@ public class GameTeam {
         if (targetPlayer == null || excludedPlayer == null) {
             return false;
         }
-        
+
         return targetPlayer.getGameTeam() == this && !targetPlayer.equals(excludedPlayer);
     }
 
     /**
      * 将玩家添加到团队
-     * 
+     *
      * @param gamePlayer 要添加的玩家
      * @return 如果添加成功返回true，否则返回false
      */
@@ -194,20 +196,20 @@ public class GameTeam {
         if (gamePlayer == null || isFull() || isInTeam(gamePlayer)) {
             return false;
         }
-        
+
         gamePlayer.setGameTeam(this);
-        
+
         // 更新玩家的TabList显示名称
         if (gamePlayer.getPlayer() != null) {
             cc.azuramc.bedwars.tablist.TabList.changeTabListNameInGame(gamePlayer);
         }
-        
+
         return true;
     }
 
     /**
      * 检查团队是否已满
-     * 
+     *
      * @return 如果团队已满返回true，否则返回false
      */
     public boolean isFull() {
@@ -216,7 +218,7 @@ public class GameTeam {
 
     /**
      * 检查团队是否已经全灭
-     * 
+     *
      * @return 如果团队没有存活玩家返回true，否则返回false
      */
     public boolean isDead() {
@@ -230,7 +232,7 @@ public class GameTeam {
 
     /**
      * 获取团队当前的活跃玩家数量
-     * 
+     *
      * @return 活跃玩家数量
      */
     public int getActivePlayerCount() {
@@ -239,22 +241,22 @@ public class GameTeam {
 
     /**
      * 检查床是否被摧毁
-     * 
+     *
      * @return 如果床被摧毁返回true，否则返回false
      */
     public boolean isBedDestroyed() {
         return isDestroyed || hasBed;
     }
-    
+
     /**
      * 设置床被摧毁状态
-     * 
+     *
      * @param destroyed 床被摧毁状态
      * @param destroyer 摧毁床的玩家，可以为null
      */
     public void setBedDestroyed(boolean destroyed, GamePlayer destroyer) {
         this.isDestroyed = destroyed;
-        
+
         if (destroyed) {
             this.destroyPlayer = destroyer;
         }
