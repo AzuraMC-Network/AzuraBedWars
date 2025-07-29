0b6c95e12299c814aabb946ffa7b820f626781e8
MindsMaster <1173348610@qq.com>
2025-07-29 19:42:12 +0800

    添加结束情况的计分板适配
    Former-commit-id: 0068fdaeec9d5a4b3502294bc7b85b35f7d938c9

--- core/src/main/java/cc/azuramc/bedwars/scoreboard/ScoreboardManager.java
+++ core/src/main/java/cc/azuramc/bedwars/scoreboard/ScoreboardManager.java
@@ -2,7 +2,8 @@ package cc.azuramc.bedwars.scoreboard;
 
 import cc.azuramc.bedwars.game.GameManager;
 import cc.azuramc.bedwars.game.GamePlayer;
-import cc.azuramc.bedwars.scoreboard.provider.GameBoardProvider;
+import cc.azuramc.bedwars.scoreboard.provider.GameEndBoardProvider;
+import cc.azuramc.bedwars.scoreboard.provider.GameRunningBoardProvider;
 import cc.azuramc.bedwars.scoreboard.provider.LobbyBoardProvider;
 import lombok.Getter;
 import org.bukkit.event.Listener;
@@ -15,85 +16,100 @@ import org.bukkit.event.Listener;
  */
 public class ScoreboardManager implements Listener {
     private final GameManager gameManager;
-    @Getter GameBoardProvider gameBoardProvider;
+    @Getter
+    GameEndBoardProvider gameEndBoardProvider;
+    @Getter
+    GameRunningBoardProvider gameRunningBoardProvider;
     @Getter LobbyBoardProvider lobbyBoardProvider;
-    
+
     /**
      * 构造函数
-     * 
+     *
      * @param gameManager 游戏管理器实例
      */
     public ScoreboardManager(GameManager gameManager) {
         this.gameManager = gameManager;
-        
+
         // 初始化提供者
-        this.gameBoardProvider = new GameBoardProvider(gameManager);
+        this.gameEndBoardProvider = new GameEndBoardProvider(gameManager);
+        this.gameRunningBoardProvider = new GameRunningBoardProvider(gameManager);
         this.lobbyBoardProvider = new LobbyBoardProvider(gameManager);
-        
+
         // 设置提供者的管理器引用
-        GameBoardProvider.setScoreboardManager(this);
+        GameEndBoardProvider.setScoreboardManager(this);
+        GameRunningBoardProvider.setScoreboardManager(this);
         LobbyBoardProvider.setScoreboardManager(this);
     }
-    
+
     /**
      * 根据游戏状态显示对应的计分板
-     * 
+     *
      * @param gamePlayer 游戏玩家
      */
     public void showBoard(GamePlayer gamePlayer) {
         if (gamePlayer == null) {
             return;
         }
-        
+
         switch (gameManager.getGameState()) {
             case RUNNING:
-                GameBoardProvider.show(gamePlayer.getPlayer());
+                GameRunningBoardProvider.show(gamePlayer.getPlayer());
                 break;
             case WAITING:
+                LobbyBoardProvider.show(gamePlayer.getPlayer());
+                break;
+            case ENDING:
+                GameEndBoardProvider.show(gamePlayer.getPlayer());
+                break;
             default:
                 LobbyBoardProvider.show(gamePlayer.getPlayer());
                 break;
         }
     }
-    
+
     /**
      * 更新所有玩家的计分板
      */
     public void updateAllBoards() {
         switch (gameManager.getGameState()) {
             case RUNNING:
-                GameBoardProvider.updateBoard();
+                GameRunningBoardProvider.updateBoard();
                 break;
             case WAITING:
+                LobbyBoardProvider.updateBoard();
+                break;
+            case ENDING:
+                GameEndBoardProvider.updateBoard();
+                break;
             default:
                 LobbyBoardProvider.updateBoard();
                 break;
         }
     }
-    
+
     /**
      * 移除玩家的计分板
-     * 
+     *
      * @param gamePlayer 游戏玩家
      */
     public void removeBoard(GamePlayer gamePlayer) {
         if (gamePlayer == null) {
             return;
         }
-        
+
         // 同时移除两种计分板，确保清理完全
-        GameBoardProvider.removeBoard(gamePlayer.getPlayer());
+        GameRunningBoardProvider.removeBoard(gamePlayer.getPlayer());
         LobbyBoardProvider.removeBoard(gamePlayer.getPlayer());
     }
-    
+
     /**
      * 移除所有玩家的计分板
      */
     public void removeAllBoards() {
-        GameBoardProvider.removeAllBoards();
+        GameRunningBoardProvider.removeAllBoards();
         LobbyBoardProvider.removeAllBoards();
     }
-    
+
     /**
      * 切换计分板模式
      * 从大厅模式切换到游戏模式，或反之
@@ -108,29 +124,30 @@ public class ScoreboardManager implements Listener {
             }
         }
     }
-    
+
     /**
      * 设置计分板更新间隔
-     * 
+     *
      * @param interval 更新间隔（毫秒）
      */
     public void setUpdateInterval(long interval) {
         if (interval > 0) {
-            GameBoardProvider.setUpdateInterval(interval);
+            GameRunningBoardProvider.setUpdateInterval(interval);
             LobbyBoardProvider.setUpdateInterval(interval);
         }
     }
-    
+
     /**
      * 初始化计分板系统
      * 注册监听器
-     * 
+     *
      * @param plugin 插件实例
      */
     public void initialize(org.bukkit.plugin.Plugin plugin) {
         // 注册监听器
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
-        plugin.getServer().getPluginManager().registerEvents(gameBoardProvider, plugin);
+        plugin.getServer().getPluginManager().registerEvents(gameEndBoardProvider, plugin);
+        plugin.getServer().getPluginManager().registerEvents(gameRunningBoardProvider, plugin);
         plugin.getServer().getPluginManager().registerEvents(lobbyBoardProvider, plugin);
     }
 
