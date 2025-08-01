1bf45842fffbe6c38c17e3745b0bb7d0c041b89b
MindsMaster <1173348610@qq.com>
2025-08-01 22:18:10 +0800

    计分板统一时间展示
    Former-commit-id: 55e2a94105d736614c4f5e56ab319d91e89dfe6a

--- core/src/main/java/cc/azuramc/bedwars/scoreboard/provider/GameEndBoardProvider.java
+++ core/src/main/java/cc/azuramc/bedwars/scoreboard/provider/GameEndBoardProvider.java
@@ -135,7 +135,7 @@ public class GameEndBoardProvider implements Listener {
         List<String> lines = new ArrayList<>();
 
         // 添加日期行
-        lines.add("§7团队 " + DATE_FORMAT.format(Calendar.getInstance().getTime()));
+        lines.add(DATE_FORMAT.format(Calendar.getInstance().getTime()));
         lines.add(EMPTY_LINE);
         // 添加事件信息
         lines.add("§c游戏结束");
