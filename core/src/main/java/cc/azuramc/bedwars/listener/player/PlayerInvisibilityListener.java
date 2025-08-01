817d88285070fd193c3d211f8c1fdf347c8aa249
MindsMaster <1173348610@qq.com>
2025-08-01 12:37:41 +0800

    修复隐身没生效的问题

--- core/src/main/java/cc/azuramc/bedwars/listener/player/PlayerInvisibilityListener.java
+++ core/src/main/java/cc/azuramc/bedwars/listener/player/PlayerInvisibilityListener.java
@@ -54,6 +54,8 @@ public class PlayerInvisibilityListener implements Listener {
             if (gamePlayer.isInvisible()) {
                 gamePlayer.endInvisibility();
             }
+
+            gamePlayer.startInvisibilityTask();
         }
     }
 
