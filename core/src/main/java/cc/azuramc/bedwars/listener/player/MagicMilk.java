be9b1fa532633911f98ee9bccf946be0893d6034
hutuy <hutuyee@outlook.com>
2025-08-03 17:15:10 +0800

    支持GamePlayer调用 移动至Listener-Player文件夹

--- core/src/main/java/cc/azuramc/bedwars/listener/player/MagicMilk.java
+++ core/src/main/java/cc/azuramc/bedwars/listener/player/MagicMilk.java
@@ -1,5 +1,6 @@
-package cc.azuramc.bedwars.listener.special;
+package cc.azuramc.bedwars.listener.player;
 
+import cc.azuramc.bedwars.game.GamePlayer;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
@@ -9,40 +10,34 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.UUID;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class MagicMilk implements Listener {
-
-    private static final Set<UUID> protectedPlayers = ConcurrentHashMap.newKeySet();
 
     @EventHandler
     public void onDrinkMilk(PlayerItemConsumeEvent event) {
         Player player = event.getPlayer();
+
+        GamePlayer gamePlayer = GamePlayer.get(player);
 
         // 判断是否喝的是牛奶
         if (event.getItem().getType() != Material.MILK_BUCKET) return;
 
         UUID uuid = player.getUniqueId();
 
-        // 添加到保护列表
-        protectedPlayers.add(uuid);
+        gamePlayer.setprotectedPlayer(true);
+
         player.sendMessage("§a你获得了 30 秒的陷阱免疫效果！");
 
         // 30秒后移除
         new BukkitRunnable() {
             @Override
             public void run() {
-                protectedPlayers.remove(uuid);
+                gamePlayer.setprotectedPlayer(false);
                 Player onlinePlayer = org.bukkit.Bukkit.getPlayer(uuid);
                 if (onlinePlayer != null) {
                     onlinePlayer.sendMessage("§c你的陷阱免疫效果已消失！");
                 }
             }
         }.runTaskLater(JavaPlugin.getProvidingPlugin(MagicMilk.class), 20 * 30); // 30s
-    }
-
-    public static boolean isTrapImmune(UUID uuid) {
-        return protectedPlayers.contains(uuid);
     }
 }
