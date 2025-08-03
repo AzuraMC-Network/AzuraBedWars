2e0259c7f878a6eba079c1684ec982b6d7bc145f
Ant1Aura <167528238+Marina-HCL@users.noreply.github.com>
2025-08-03 18:44:25 +0800

    Revert "快捷存储"
    Former-commit-id: 38bc7e7147bc5fe5d8885d561b43f436d272ed3d

--- core/src/main/java/cc/azuramc/bedwars/listener/player/PlayerResourcePutListener.java
+++ core/src/main/java/cc/azuramc/bedwars/listener/player/PlayerResourcePutListener.java
@@ -30,11 +30,10 @@ public class PlayerResourcePutListener implements Listener {
         Player player = event.getPlayer();
         GamePlayer gamePlayer = GamePlayer.get(player);
         Action action = event.getAction();
-        ItemStack item = event.getItem();
 
         if (gamePlayer == null
                 || gameManager.getGameState() != GameState.RUNNING
-                || item == null
+                || event.getItem() == null
                 || action != Action.LEFT_CLICK_BLOCK) {
             return;
         }
@@ -65,6 +64,19 @@ public class PlayerResourcePutListener implements Listener {
 
         if (targetInventory == null) return;
 
+        ItemStack item = event.getItem();
+        ItemStack resource;
+        switch (item.getType()) {
+            case IRON_INGOT:
+            case GOLD_INGOT:
+            case DIAMOND:
+            case EMERALD:
+                resource = item;
+                break;
+            default:
+                return;
+        }
+
         Inventory inventory = player.getInventory();
         boolean hasFailures = false;
         int transferredCount = 0;
@@ -72,7 +84,7 @@ public class PlayerResourcePutListener implements Listener {
 
         for (int i = 0; i < inventory.getSize(); i++) {
             ItemStack item1 = inventory.getItem(i);
-            if (item1 != null && item1.getType() == item.getType()) {
+            if (item1 != null && item1.getType() == resource.getType()) {
                 totalItems += item1.getAmount();
                 HashMap<Integer, ItemStack> left = targetInventory.addItem(item1.clone());
 
