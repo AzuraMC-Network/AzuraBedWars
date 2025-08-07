e2f5c0dbb30e4446d029ec78f622ad9328367685
MindsMaster <1173348610@qq.com>
2025-08-08 01:39:44 +0800

    feat: add dashboard module

---
+++ azurabedwars-dashboard/src/test/java/cc/azuramc/azurabedwarsdashboard/AzurabedwarsDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.azuramc.azurabedwarsdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurabedwarsDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
