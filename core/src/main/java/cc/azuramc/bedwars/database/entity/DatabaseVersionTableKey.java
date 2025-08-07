c0dd0f307152faa0001f3ca7beb26f68ff325ec1
MindsMaster <1173348610@qq.com>
2025-08-08 00:16:01 +0800

    feat: add database schema versioning system and add ties finalDeaths in PlayerData
    Former-commit-id: 0046632828c1935bbbe3b97da0fd04aebbe66fa3

---
+++ core/src/main/java/cc/azuramc/bedwars/database/entity/DatabaseVersionTableKey.java
@@ -0,0 +1,9 @@
+package cc.azuramc.bedwars.database.entity;
+
+/**
+ * @author An5w1r@163.com
+ */
+public class DatabaseVersionTableKey {
+    public static final String tableName = "database_version";
+    public static final String version = "version";
+}
