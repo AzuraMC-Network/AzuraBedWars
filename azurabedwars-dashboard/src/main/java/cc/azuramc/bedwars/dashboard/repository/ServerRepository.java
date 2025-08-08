8fdd8f5a0626cf9bf0fddae3cdc0ed49547cb2fb
MindsMaster <1173348610@qq.com>
2025-08-08 17:32:57 +0800

    feat: add method find server by display name

--- azurabedwars-dashboard/src/main/java/cc/azuramc/bedwars/dashboard/repository/ServerRepository.java
+++ azurabedwars-dashboard/src/main/java/cc/azuramc/bedwars/dashboard/repository/ServerRepository.java
@@ -4,9 +4,13 @@ import cc.azuramc.bedwars.dashboard.entity.Server;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * @author An5w1r@163.com
  */
 @Repository
 public interface ServerRepository extends CrudRepository<Server, Integer> {
+
+    Optional<Server> findByDisplayName(String displayName);
 }
