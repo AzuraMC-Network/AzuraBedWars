f6a37ddaae164cc2d98c86b9dfa8b159916b3c8c
MindsMaster <1173348610@qq.com>
2025-09-07 23:21:19 +0800

    fix: refactor PlayerData data interaction classes
    - Removed incorrect design of fetching ID first before reading full data
    - Fixed type mismatch in selectPlayerDataByUuid where experience used rs.getInt()
    - Added automatic updatedAt update logic to all PlayerData add methods
    - Fixed insertPlayerData to include createdAt and updatedAt on insert
    - Fixed updatePlayerData to update updatedAt on modification
    - Ensured createdAt is set on insert and updatedAt auto-updates on every data change

--- azurabedwars-plugin/core/src/main/java/cc/azuramc/bedwars/database/entity/PlayerData.java
+++ azurabedwars-plugin/core/src/main/java/cc/azuramc/bedwars/database/entity/PlayerData.java
@@ -3,7 +3,8 @@ package cc.azuramc.bedwars.database.entity;
 import cc.azuramc.bedwars.game.GameModeType;
 import cc.azuramc.bedwars.game.GamePlayer;
 import cc.azuramc.bedwars.game.level.PlayerLevelManager;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -11,7 +12,8 @@ import java.util.UUID;
 /**
  * @author an5w1r@163.com
  */
-@Data
+@Getter
+@Setter
 public class PlayerData {
 
     private int id;
@@ -61,109 +63,132 @@ public class PlayerData {
 
     public void addLevel(int level) {
         this.level += level;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addExperience(double experience) {
         this.experience += experience;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addKills(int kills) {
         gamePlayer.addCurrentGameKills(kills);
         this.kills += kills;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addDeaths(int deaths) {
         gamePlayer.addCurrentGameDeaths(deaths);
         this.deaths += deaths;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addAssists(int assists) {
         gamePlayer.addCurrentGameAssists(assists);
         this.assists += assists;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addFinalKills(int finalKills) {
         gamePlayer.addCurrentGameFinalKills(finalKills);
         this.finalKills += finalKills;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addFinalDeaths(int finalDeaths) {
         this.finalDeaths += finalDeaths;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addDestroyedBeds(int destroyedBeds) {
         gamePlayer.addCurrentGameDestroyedBeds(destroyedBeds);
         this.destroyedBeds += destroyedBeds;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addWins(int wins) {
         this.wins += wins;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addTies(int ties) {
         this.ties += ties;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addLosses(int losses) {
         this.losses += losses;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addGames(int games) {
         this.games += games;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addLevel() {
         this.level++;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addKills() {
         PlayerLevelManager.addExperience(this.gamePlayer, 1);
         gamePlayer.addCurrentGameKills();
         this.kills++;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addDeaths() {
         gamePlayer.addCurrentGameDeaths();
         this.deaths++;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addAssists() {
         PlayerLevelManager.addExperience(this.gamePlayer, 0.5);
         gamePlayer.addCurrentGameAssists();
         this.assists++;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addFinalKills() {
         PlayerLevelManager.addExperience(this.gamePlayer, 2);
         gamePlayer.addCurrentGameFinalKills();
         this.finalKills++;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addFinalDeaths() {
         this.finalDeaths++;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addDestroyedBeds() {
         PlayerLevelManager.addExperience(this.gamePlayer, 3);
         gamePlayer.addCurrentGameDestroyedBeds();
         this.destroyedBeds++;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addWins() {
         PlayerLevelManager.addExperience(this.gamePlayer, 5);
         this.wins++;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addTies() {
         this.ties++;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addLosses() {
         this.losses++;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 
     public void addGames() {
         PlayerLevelManager.addExperience(this.gamePlayer, 0.1);
         this.games++;
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
     }
 }
