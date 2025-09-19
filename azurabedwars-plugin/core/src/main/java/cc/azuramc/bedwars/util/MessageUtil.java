c114b1c05500ee245a15640bfc59991f17654b75
MindsMaster <1173348610@qq.com>
2025-09-20 00:43:55 +0800

    refactor(command): improve message sending format

--- azurabedwars-plugin/core/src/main/java/cc/azuramc/bedwars/util/MessageUtil.java
+++ azurabedwars-plugin/core/src/main/java/cc/azuramc/bedwars/util/MessageUtil.java
@@ -1,12 +1,15 @@
 package cc.azuramc.bedwars.util;
 
+import cc.azuramc.bedwars.game.map.MapData;
 import com.cryptomorin.xseries.messages.ActionBar;
 import com.cryptomorin.xseries.messages.Titles;
 import lombok.Setter;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +17,9 @@ import java.util.List;
  * @author an5w1r@163.com
  */
 public final class MessageUtil {
+
+    private static final DecimalFormat COORDINATE_FORMAT = new DecimalFormat("0.00");
+    private static final DecimalFormat ANGLE_FORMAT = new DecimalFormat("0.0");
 
     @Setter
     private static boolean usingPlaceholderAPI = false;
@@ -110,6 +116,74 @@ public final class MessageUtil {
         }
 
         return ChatColor.RESET;
+    }
+
+    /**
+     * 格式化Location的显示格式
+     * 格式: 世界名: X.XX, Y.YY, Z.ZZ (Yaw: XX.X, Pitch: XX.X)
+     *
+     * @param location 要格式化的位置
+     * @return 格式化后的字符串，如果location为null则返回"未设置"
+     */
+    public static String formatLocation(Location location) {
+        if (location == null) {
+            return "&c未设置";
+        }
+
+        String worldName = location.getWorld() != null ? location.getWorld().getName() : "未知世界";
+        String x = COORDINATE_FORMAT.format(location.getX());
+        String y = COORDINATE_FORMAT.format(location.getY());
+        String z = COORDINATE_FORMAT.format(location.getZ());
+        String yaw = ANGLE_FORMAT.format(location.getYaw());
+        String pitch = ANGLE_FORMAT.format(location.getPitch());
+
+        return String.format("&b%s&f: &b%s, %s, %s &7(Yaw: %s, Pitch: %s)",
+                worldName, x, y, z, yaw, pitch);
+    }
+
+    /**
+     * 格式化RawLocation的显示格式
+     * 格式: 世界名: X.XX, Y.YY, Z.ZZ (Yaw: XX.X, Pitch: XX.X)
+     *
+     * @param rawLocation 要格式化的原始位置
+     * @return 格式化后的字符串，如果rawLocation为null则返回"未设置"
+     */
+    public static String formatLocation(MapData.RawLocation rawLocation) {
+        if (rawLocation == null) {
+            return "&c未设置";
+        }
+
+        String worldName = rawLocation.getWorld() != null ? rawLocation.getWorld() : "未知世界";
+        String x = COORDINATE_FORMAT.format(rawLocation.getX());
+        String y = COORDINATE_FORMAT.format(rawLocation.getY());
+        String z = COORDINATE_FORMAT.format(rawLocation.getZ());
+        String yaw = ANGLE_FORMAT.format(rawLocation.getYaw());
+        String pitch = ANGLE_FORMAT.format(rawLocation.getPitch());
+
+        return String.format("&b%s&f: &b%s, %s, %s &7(Yaw: %s, Pitch: %s)",
+                worldName, x, y, z, yaw, pitch);
+    }
+
+    /**
+     * 格式化RawLocation列表为友好的显示格式
+     * 格式: 基地1: 世界名: X.XX, Y.YY, Z.ZZ (Yaw: XX.X, Pitch: XX.X)
+     * 基地2: 世界名: X.XX, Y.YY, Z.ZZ (Yaw: XX.X, Pitch: XX.X)
+     *
+     * @param locations RawLocation列表
+     * @return 格式化后的字符串列表，如果列表为空则返回包含"无"的单元素列表
+     */
+    public static List<String> formatLocationList(List<MapData.RawLocation> locations) {
+        if (locations == null || locations.isEmpty()) {
+            return List.of("&c无");
+        }
+
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < locations.size(); i++) {
+            MapData.RawLocation location = locations.get(i);
+            result.add(String.format("    &7- 基地%d: %s", i + 1, formatLocation(location)));
+        }
+
+        return result;
     }
 
 }
