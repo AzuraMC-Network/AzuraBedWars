c980784c98136727ec04f6059b9f9ca981785a0c
MindsMaster <1173348610@qq.com>
2025-09-25 23:25:44 +0800

    refactor(nms): implement inheritance pattern for custom entities
    - Add `AbstractCustomEntity` base class with common fields and methods
    - Extract target validation logic (`isValidPlayerTarget`, `isValidIronGolemTarget`, `isValidSilverfishTarget`)
    - Refactor `CustomIronGolem` and `CustomSilverfish` to inherit from base class
    - Replace lambda expressions with cleaner method references
    - Reduce code duplication and improve extensibility for future custom entities
    
    
    Former-commit-id: 69a4f10ab71ce224261e1ecefdb9ec6b61826628

---
+++ azurabedwars-plugin/1_8_R3/src/main/java/cc/azuramc/bedwars/nms/v1_8_R3/AbstractCustomEntity.java
@@ -0,0 +1,120 @@
+package cc.azuramc.bedwars.nms.v1_8_R3;
+
+import cc.azuramc.bedwars.game.GamePlayer;
+import cc.azuramc.bedwars.game.GameTeam;
+import lombok.Getter;
+import net.minecraft.server.v1_8_R3.*;
+import org.bukkit.craftbukkit.v1_8_R3.util.UnsafeList;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author an5w1r@163.com
+ */
+@Getter
+public abstract class AbstractCustomEntity {
+    @Getter
+    private static ConcurrentHashMap<EntityInsentient, AbstractCustomEntity> customEntityMap = new ConcurrentHashMap<>();
+
+    protected EntityInsentient entityInsentient;
+    protected EntityCreature entityCreature;
+    protected GameTeam gameTeam;
+
+    protected AbstractCustomEntity(EntityInsentient entityInsentient, GameTeam gameTeam) {
+        if (entityInsentient == null || gameTeam == null) {
+            return;
+        }
+
+        this.entityInsentient = entityInsentient;
+        this.entityCreature = (EntityCreature) entityInsentient;
+        this.gameTeam = gameTeam;
+
+        clearGoals();
+        setupAttributes();
+        setupGoals();
+        setupTargets();
+    }
+
+    protected void clearGoals() {
+        try {
+            Field b = PathfinderGoalSelector.class.getDeclaredField("b");
+            Field c = PathfinderGoalSelector.class.getDeclaredField("c");
+            b.setAccessible(true);
+            c.setAccessible(true);
+            b.set(entityInsentient.goalSelector, new UnsafeList<>());
+            c.set(entityInsentient.goalSelector, new UnsafeList<>());
+            b.set(entityInsentient.targetSelector, new UnsafeList<>());
+            c.set(entityInsentient.targetSelector, new UnsafeList<>());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void setupAttributes() {
+    }
+
+    protected abstract void setupGoals();
+
+    protected abstract void setupTargets();
+
+    /**
+     * 判断玩家是否为有效攻击目标
+     *
+     * @param human 玩家实体
+     * @return 是否为有效目标
+     */
+    protected boolean isValidPlayerTarget(EntityHuman human) {
+        if (human == null || !human.isAlive()) {
+            return false;
+        }
+
+        GamePlayer gamePlayer = GamePlayer.get(human.getUniqueID());
+        if (gamePlayer == null) {
+            return false;
+        }
+
+        // 不攻击同队玩家和观察者
+        return !gameTeam.isInTeam(gamePlayer) && !gamePlayer.isSpectator();
+    }
+
+    /**
+     * 判断铁傀儡是否为有效攻击目标
+     *
+     * @param golem 铁傀儡实体
+     * @return 是否为有效目标
+     */
+    protected boolean isValidIronGolemTarget(EntityIronGolem golem) {
+        if (golem == null) {
+            return false;
+        }
+
+        AbstractCustomEntity customGolem = AbstractCustomEntity.getCustomEntityMap().get(golem);
+        if (customGolem == null) {
+            return false;
+        }
+
+        // 不攻击同队的铁傀儡
+        return customGolem.getGameTeam() != gameTeam;
+    }
+
+    /**
+     * 判断蠹虫是否为有效攻击目标
+     *
+     * @param fish 蠹虫实体
+     * @return 是否为有效目标
+     */
+    protected boolean isValidSilverfishTarget(EntitySilverfish fish) {
+        if (fish == null) {
+            return false;
+        }
+
+        AbstractCustomEntity customFish = AbstractCustomEntity.getCustomEntityMap().get(fish);
+        if (customFish == null) {
+            return false;
+        }
+
+        // 不攻击同队的蠹虫
+        return customFish.getGameTeam() != gameTeam;
+    }
+}
