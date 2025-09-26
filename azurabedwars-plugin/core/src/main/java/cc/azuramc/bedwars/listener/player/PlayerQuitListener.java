a13ef94f8293f8a42bfaa56175f284883c02225b
MindsMaster <1173348610@qq.com>
2025-09-27 00:26:30 +0800

    fix: game end player quit cause NPE
    Former-commit-id: 1317ac448d6b2e3287faf359fc01aab256be4b0a

--- azurabedwars-plugin/core/src/main/java/cc/azuramc/bedwars/listener/player/PlayerQuitListener.java
+++ azurabedwars-plugin/core/src/main/java/cc/azuramc/bedwars/listener/player/PlayerQuitListener.java
@@ -3,6 +3,7 @@ package cc.azuramc.bedwars.listener.player;
 import cc.azuramc.bedwars.AzuraBedWars;
 import cc.azuramc.bedwars.game.GameManager;
 import cc.azuramc.bedwars.game.GamePlayer;
+import cc.azuramc.bedwars.tablist.TabListManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -13,6 +14,7 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class PlayerQuitListener implements Listener {
 
     private final GameManager gameManager = AzuraBedWars.getInstance().getGameManager();
+    private final TabListManager tabListManager = gameManager.getTabListManager();
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
@@ -20,6 +22,8 @@ public class PlayerQuitListener implements Listener {
         GamePlayer gamePlayer = GamePlayer.get(event.getPlayer());
         if (gamePlayer != null) {
             gamePlayer.endInvisibility();
+
+            tabListManager.removePlayerFromTab(gamePlayer);
             gameManager.removePlayers(gamePlayer);
         }
         AzuraBedWars.getInstance().getScoreboardManager().updateAllBoards();
