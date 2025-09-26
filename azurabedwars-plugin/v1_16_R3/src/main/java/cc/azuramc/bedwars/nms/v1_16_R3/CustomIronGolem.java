92778f0d1d77486f6fcc02ec7a47b03289f945a3
MindsMaster <1173348610@qq.com>
2025-09-27 01:20:17 +0800

    feat(nms): add support for multiple versions
    - v1_15_R1 (1.15–1.15.2)
    - v1_16_R1 (1.16.1)
    - v1_16_R2 (1.16.2–1.16.3)
    - v1_16_R3 (1.16.4–1.16.5)
    - v1_18_R1 (1.18–1.18.1)
    - v1_18_R2 (1.18.2)
    - v1_19_R1 (1.19–1.19.2)
    - v1_19_R2 (1.19.3)
    - v1_19_R3 (1.19.4)
    
    
    Former-commit-id: 09e0e3194693c4c3985389f8f9b3b202e06cd623

---
+++ azurabedwars-plugin/v1_16_R3/src/main/java/cc/azuramc/bedwars/nms/v1_16_R3/CustomIronGolem.java
@@ -0,0 +1,69 @@
+package cc.azuramc.bedwars.nms.v1_16_R3;
+
+import cc.azuramc.bedwars.game.GameTeam;
+import net.minecraft.server.v1_16_R3.*;
+import org.bukkit.Location;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftLivingEntity;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+
+import java.util.Objects;
+
+/**
+ * @author an5w1r@163.com
+ */
+public class CustomIronGolem extends AbstractCustomEntity {
+
+    protected CustomIronGolem(EntityInsentient entityInsentient, GameTeam gameTeam) {
+        super(entityInsentient, gameTeam);
+    }
+
+    public static LivingEntity spawn(Location loc, GameTeam gameTeam, double speed, double health) {
+        Entity bukkitEntity = EntityConverter.spawnBukkitEntity(loc, EntityType.IRON_GOLEM);
+        CraftLivingEntity craftLivingEntity = (CraftLivingEntity) bukkitEntity;
+        EntityInsentient entityInsentient = EntityConverter.bukkitToNms(bukkitEntity);
+
+        if (entityInsentient == null) {
+            return null;
+        }
+
+        getCustomEntityMap().put(entityInsentient, new CustomIronGolem(entityInsentient, gameTeam));
+
+        LivingEntity bukkitLivingEntity = (LivingEntity) bukkitEntity;
+        Objects.requireNonNull(bukkitLivingEntity.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED), "Spawn IronGolem speed parameter is null").setBaseValue(speed);
+        Objects.requireNonNull(bukkitLivingEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH), "Spawn IronGolem health parameter is null").setBaseValue(health);
+        entityInsentient.setCustomNameVisible(true);
+        craftLivingEntity.setRemoveWhenFarAway(false);
+        return craftLivingEntity;
+    }
+
+    @Override
+    protected void setupGoals() {
+        entityInsentient.goalSelector.a(1, new PathfinderGoalFloat(entityInsentient));
+        entityInsentient.goalSelector.a(2, new PathfinderGoalMeleeAttack(entityCreature, 1.5D, false));
+        entityInsentient.goalSelector.a(3, new PathfinderGoalMoveTowardsTarget(entityCreature, 1.0D, 20.0F));
+        entityInsentient.goalSelector.a(4, new PathfinderGoalRandomStroll(entityCreature, 1D));
+        entityInsentient.goalSelector.a(5, new PathfinderGoalRandomLookaround(entityCreature));
+    }
+
+    @Override
+    protected void setupTargets() {
+        // 被攻击时立即反击
+        entityInsentient.targetSelector.a(1, new PathfinderGoalHurtByTarget(entityCreature));
+
+        // 主动寻找玩家目标
+        entityInsentient.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget<>(entityCreature, EntityHuman.class, 20, true, false,
+                this::isValidPlayerTarget));
+
+        // 主动寻找铁傀儡目标
+        entityInsentient.targetSelector.a(3, new PathfinderGoalNearestAttackableTarget<>(entityCreature, EntityIronGolem.class, 20, true, false,
+                this::isValidIronGolemTarget));
+
+        // 主动寻找其他蠹虫目标
+        entityInsentient.targetSelector.a(4, new PathfinderGoalNearestAttackableTarget<>(entityCreature, EntitySilverfish.class, 20, true, false,
+                this::isValidSilverfishTarget));
+    }
+
+}
---
+++ azurabedwars-plugin/v1_19_R2/src/main/java/cc/azuramc/bedwars/nms/v1_19_R2/NMS_v1_19_R2.java
@@ -0,0 +1,41 @@
+package cc.azuramc.bedwars.nms.v1_19_R2;
+
+import cc.azuramc.bedwars.game.GamePlayer;
+import cc.azuramc.bedwars.game.GameTeam;
+import cc.azuramc.bedwars.nms.NMSAccess;
+import cc.azuramc.bedwars.util.LoggerUtil;
+import net.minecraft.world.entity.projectile.EntityFireball;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_19_R2.entity.CraftFireball;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.util.Vector;
+
+/**
+ * @author an5w1r@163.com
+ */
+public class NMS_v1_19_R2 implements NMSAccess {
+
+    @Override
+    public Fireball setFireballDirection(Fireball fireball, Vector vector) {
+        EntityFireball fb = ((CraftFireball) fireball).getHandle();
+        fb.b = vector.getX() * 0.1D;
+        fb.c = vector.getY() * 0.1D;
+        fb.d = vector.getZ() * 0.1D;
+        return (Fireball) fb.getBukkitEntity();
+    }
+
+    @Override
+    public LivingEntity spawnIronGolem(Location loc, GamePlayer gamePlayer, double speed, double health) {
+        GameTeam gameTeam = gamePlayer.getGameTeam();
+        LoggerUtil.debug("NMS_v1_19_R2$spawnIronGolem | loc: " + loc + ", gameTeam: " + gamePlayer.getName() + ", speed: " + speed + ", health: " + health);
+        return CustomIronGolem.spawn(loc, gameTeam, speed, health);
+    }
+
+    @Override
+    public LivingEntity spawnSilverfish(Location loc, GamePlayer gamePlayer, double speed, double health) {
+        GameTeam gameTeam = gamePlayer.getGameTeam();
+        LoggerUtil.debug("NMS_v1_19_R2$spawnSilverfish | loc: " + loc + ", gameTeam: " + gameTeam.getName() + ", speed: " + speed + ", health: " + health);
+        return CustomSilverfish.spawn(loc, gameTeam, speed, health);
+    }
+}
