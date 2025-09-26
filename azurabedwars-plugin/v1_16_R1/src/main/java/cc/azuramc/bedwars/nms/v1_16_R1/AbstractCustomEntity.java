a558f9b65e5f769dd1f19f099d5c90bb01b2f098
MindsMaster <1173348610@qq.com>
2025-09-27 01:20:17 +0800

    feat(nms): add support for multiple versions
    - v1_15_R1 (1.15–1.15.2)
    - v1_16_R1 (1.16.1)
    - v1_16_R2 (1.16.2–1.16.3)
    - v1_16_R3 (1.16.4–1.16.5)
    - v1_18_R1 (1.18–1.18.1)
    - v1_18_R2 (1.18.2)
    - v1_19_R1 (1.19–1.19.2)
    - v1_19_R2 (1.19.3)
    - v1_19_R3 (1.19.4)

---
+++ azurabedwars-plugin/v1_16_R1/src/main/java/cc/azuramc/bedwars/nms/v1_16_R1/AbstractCustomEntity.java
@@ -0,0 +1,129 @@
+package cc.azuramc.bedwars.nms.v1_16_R1;
+
+import cc.azuramc.bedwars.game.GamePlayer;
+import cc.azuramc.bedwars.game.GameTeam;
+import com.google.common.collect.Sets;
+import lombok.Getter;
+import net.minecraft.server.v1_16_R1.*;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author an5w1r@163.com
+ */
+@Getter
+public abstract class AbstractCustomEntity {
+    @Getter
+    private static ConcurrentHashMap<EntityInsentient, AbstractCustomEntity> customEntityMap = new ConcurrentHashMap<>();
+
+    private static Field pathfinderGoalWrappedSet;
+    private static boolean reflectionInitialized = false;
+
+    protected EntityInsentient entityInsentient;
+    protected EntityCreature entityCreature;
+    protected GameTeam gameTeam;
+
+    protected AbstractCustomEntity(EntityInsentient entityInsentient, GameTeam gameTeam) {
+        if (entityInsentient == null || gameTeam == null) {
+            return;
+        }
+
+        this.entityInsentient = entityInsentient;
+        this.entityCreature = (EntityCreature) entityInsentient;
+        this.gameTeam = gameTeam;
+
+        clearGoals();
+        setupAttributes();
+        setupGoals();
+        setupTargets();
+    }
+
+    protected static void initializeReflection() {
+        try {
+            pathfinderGoalWrappedSet = PathfinderGoalSelector.class.getDeclaredField("d");
+            pathfinderGoalWrappedSet.setAccessible(true);
+            reflectionInitialized = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            reflectionInitialized = false;
+        }
+    }
+
+    protected void setupAttributes() {
+    }
+
+    protected abstract void setupGoals();
+
+    protected abstract void setupTargets();
+
+    /**
+     * 判断玩家是否为有效攻击目标
+     *
+     * @param entity 实体
+     * @return 是否为有效目标
+     */
+    protected boolean isValidPlayerTarget(EntityLiving entity) {
+        if (!(entity instanceof EntityHuman human)) {
+            return false;
+        }
+
+        if (human.getBukkitEntity().isDead()) {
+            return false;
+        }
+
+        GamePlayer gamePlayer = GamePlayer.get(human.getBukkitEntity().getUniqueId());
+        return !gameTeam.isInTeam(gamePlayer) && !gamePlayer.isSpectator();
+    }
+
+    /**
+     * 判断铁傀儡是否为有效攻击目标
+     *
+     * @param entity 实体
+     * @return 是否为有效目标
+     */
+    protected boolean isValidIronGolemTarget(EntityLiving entity) {
+        if (!(entity instanceof EntityIronGolem golem)) {
+            return false;
+        }
+
+        if (!AbstractCustomEntity.getCustomEntityMap().containsKey(golem)) {
+            return false;
+        }
+
+        AbstractCustomEntity customGolem = AbstractCustomEntity.getCustomEntityMap().get(golem);
+        return customGolem.getGameTeam() != gameTeam;
+    }
+
+    /**
+     * 判断蠹虫是否为有效攻击目标
+     *
+     * @param entity 实体
+     * @return 是否为有效目标
+     */
+    protected boolean isValidSilverfishTarget(EntityLiving entity) {
+        if (!(entity instanceof EntitySilverfish fish)) {
+            return false;
+        }
+
+        if (!AbstractCustomEntity.getCustomEntityMap().containsKey(fish)) {
+            return false;
+        }
+
+        AbstractCustomEntity customFish = AbstractCustomEntity.getCustomEntityMap().get(fish);
+        return customFish.getGameTeam() != gameTeam;
+    }
+
+    protected void clearGoals() {
+        if (!reflectionInitialized) {
+            return;
+        }
+
+        try {
+            pathfinderGoalWrappedSet.set(entityInsentient.goalSelector, Sets.newLinkedHashSet());
+            pathfinderGoalWrappedSet.set(entityInsentient.targetSelector, Sets.newLinkedHashSet());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ azurabedwars-plugin/v1_16_R3/src/main/java/cc/azuramc/bedwars/nms/v1_16_R3/NMS_v1_16_R3.java
@@ -0,0 +1,45 @@
+package cc.azuramc.bedwars.nms.v1_16_R3;
+
+import cc.azuramc.bedwars.game.GamePlayer;
+import cc.azuramc.bedwars.game.GameTeam;
+import cc.azuramc.bedwars.nms.NMSAccess;
+import cc.azuramc.bedwars.util.LoggerUtil;
+import net.minecraft.server.v1_16_R3.EntityFireball;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftFireball;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.util.Vector;
+
+/**
+ * @author an5w1r@163.com
+ */
+public class NMS_v1_16_R3 implements NMSAccess {
+
+    public NMS_v1_16_R3() {
+        AbstractCustomEntity.initializeReflection();
+    }
+
+    @Override
+    public Fireball setFireballDirection(Fireball fireball, Vector vector) {
+        EntityFireball fb = ((CraftFireball) fireball).getHandle();
+        fb.dirX = vector.getX() * 0.1D;
+        fb.dirY = vector.getY() * 0.1D;
+        fb.dirZ = vector.getZ() * 0.1D;
+        return (Fireball) fb.getBukkitEntity();
+    }
+
+    @Override
+    public LivingEntity spawnIronGolem(Location loc, GamePlayer gamePlayer, double speed, double health) {
+        GameTeam gameTeam = gamePlayer.getGameTeam();
+        LoggerUtil.debug("NMS_v1_16_R3$spawnIronGolem | loc: " + loc + ", gameTeam: " + gamePlayer.getName() + ", speed: " + speed + ", health: " + health);
+        return CustomIronGolem.spawn(loc, gameTeam, speed, health);
+    }
+
+    @Override
+    public LivingEntity spawnSilverfish(Location loc, GamePlayer gamePlayer, double speed, double health) {
+        GameTeam gameTeam = gamePlayer.getGameTeam();
+        LoggerUtil.debug("NMS_v1_16_R3$spawnSilverfish | loc: " + loc + ", gameTeam: " + gameTeam.getName() + ", speed: " + speed + ", health: " + health);
+        return CustomSilverfish.spawn(loc, gameTeam, speed, health);
+    }
+}
