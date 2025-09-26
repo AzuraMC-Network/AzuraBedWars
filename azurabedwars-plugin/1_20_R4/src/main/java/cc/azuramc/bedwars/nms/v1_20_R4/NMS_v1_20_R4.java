f75f33bf343a498bf00bb26e401aead92638dbc2
MindsMaster <1173348610@qq.com>
2025-09-26 10:32:29 +0800

    feat: add v1_20_R4(1.20.6) nms support
    Former-commit-id: 0cfabf1da87f68d36fcfb8fe274d705e64c82d9b

---
+++ azurabedwars-plugin/1_20_R4/src/main/java/cc/azuramc/bedwars/nms/v1_20_R4/NMS_v1_20_R4.java
@@ -0,0 +1,41 @@
+package cc.azuramc.bedwars.nms.v1_20_R4;
+
+import cc.azuramc.bedwars.game.GamePlayer;
+import cc.azuramc.bedwars.game.GameTeam;
+import cc.azuramc.bedwars.nms.NMSAccess;
+import cc.azuramc.bedwars.util.LoggerUtil;
+import net.minecraft.world.entity.projectile.EntityFireball;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_20_R4.entity.CraftFireball;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.util.Vector;
+
+/**
+ * @author an5w1r@163.com
+ */
+public class NMS_v1_20_R4 implements NMSAccess {
+
+    @Override
+    public Fireball setFireballDirection(Fireball fireball, Vector vector) {
+        EntityFireball fb = ((CraftFireball) fireball).getHandle();
+        fb.d = vector.getX() * 0.1D;
+        fb.e = vector.getY() * 0.1D;
+        fb.f = vector.getZ() * 0.1D;
+        return (Fireball) fb.getBukkitEntity();
+    }
+
+    @Override
+    public LivingEntity spawnIronGolem(Location loc, GamePlayer gamePlayer, double speed, double health, double damage) {
+        GameTeam gameTeam = gamePlayer.getGameTeam();
+        LoggerUtil.debug("NMS_v1_20_R4$spawnIronGolem | loc: " + loc + ", gameTeam: " + gamePlayer.getName() + ", speed: " + speed + ", health: " + health);
+        return CustomIronGolem.spawn(loc, gameTeam, speed, health, damage);
+    }
+
+    @Override
+    public LivingEntity spawnSilverfish(Location loc, GamePlayer gamePlayer, double speed, double health, double damage) {
+        GameTeam gameTeam = gamePlayer.getGameTeam();
+        LoggerUtil.debug("NMS_v1_20_R4$spawnSilverfish | loc: " + loc + ", gameTeam: " + gameTeam.getName() + ", speed: " + speed + ", health: " + health + ", damage: " + damage);
+        return CustomSilverfish.spawn(loc, gameTeam, speed, health, damage);
+    }
+}
