9fe8a804dd7d861e235b8a2ceb5b0b5dd1f24a6f
MindsMaster <1173348610@qq.com>
2025-09-26 13:03:11 +0800

    feat: add v1_13_R1(1.13), v1_13_R2(1.13.1-1.13.2) nms support
    Former-commit-id: 1bc82517acc04de00fd3d5c26eed99a916becf10

---
+++ azurabedwars-plugin/v1_13_R2/src/main/java/cc/azuramc/bedwars/nms/v1_13_R2/CustomSilverfish.java
@@ -0,0 +1,69 @@
+package cc.azuramc.bedwars.nms.v1_13_R2;
+
+import cc.azuramc.bedwars.game.GameTeam;
+import net.minecraft.server.v1_13_R2.*;
+import org.bukkit.Location;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.craftbukkit.v1_13_R2.entity.CraftLivingEntity;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+
+import java.util.Objects;
+
+/**
+ * @author an5w1r@163.com
+ */
+public class CustomSilverfish extends AbstractCustomEntity {
+
+    protected CustomSilverfish(EntityInsentient entityInsentient, GameTeam gameTeam) {
+        super(entityInsentient, gameTeam);
+    }
+
+    public static LivingEntity spawn(Location loc, GameTeam gameTeam, double speed, double health, double damage) {
+        Entity bukkitEntity = EntityConverter.spawnBukkitEntity(loc, EntityType.SILVERFISH);
+        CraftLivingEntity craftLivingEntity = (CraftLivingEntity) bukkitEntity;
+        EntityInsentient entityInsentient = EntityConverter.bukkitToNms(bukkitEntity);
+
+        if (entityInsentient == null) {
+            return null;
+        }
+
+        getCustomEntityMap().put(entityInsentient, new CustomSilverfish(entityInsentient, gameTeam));
+
+        LivingEntity bukkitLivingEntity = (LivingEntity) bukkitEntity;
+        Objects.requireNonNull(bukkitLivingEntity.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED), "Spawn Silverfish speed parameter is null").setBaseValue(speed);
+        Objects.requireNonNull(bukkitLivingEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH), "Spawn Silverfish health parameter is null").setBaseValue(health);
+        Objects.requireNonNull(bukkitLivingEntity.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE), "Spawn Silverfish damage parameter is null").setBaseValue(damage);
+        entityInsentient.setCustomNameVisible(true);
+        craftLivingEntity.setRemoveWhenFarAway(false);
+        return craftLivingEntity;
+    }
+
+    @Override
+    protected void setupGoals() {
+        entityInsentient.goalSelector.a(1, new PathfinderGoalFloat(entityInsentient));
+        entityInsentient.goalSelector.a(2, new PathfinderGoalMeleeAttack(entityCreature, 1.9D, false));
+        entityInsentient.goalSelector.a(3, new PathfinderGoalMoveTowardsTarget(entityCreature, 1.0D, 20.0F));
+        entityInsentient.goalSelector.a(4, new PathfinderGoalRandomStroll(entityCreature, 1.5D));
+        entityInsentient.goalSelector.a(5, new PathfinderGoalRandomLookaround(entityCreature));
+    }
+
+    @Override
+    protected void setupTargets() {
+        // 被攻击时立即反击
+        entityInsentient.targetSelector.a(1, new PathfinderGoalHurtByTarget(entityCreature, true));
+
+        // 主动寻找玩家目标
+        entityInsentient.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget<>(entityCreature, EntityHuman.class, 20, true, false,
+                this::isValidPlayerTarget));
+
+        // 主动寻找铁傀儡目标
+        entityInsentient.targetSelector.a(3, new PathfinderGoalNearestAttackableTarget<>(entityCreature, EntityIronGolem.class, 20, true, false,
+                this::isValidIronGolemTarget));
+
+        // 主动寻找其他蠹虫目标
+        entityInsentient.targetSelector.a(4, new PathfinderGoalNearestAttackableTarget<>(entityCreature, EntitySilverfish.class, 20, true, false,
+                this::isValidSilverfishTarget));
+    }
+}
