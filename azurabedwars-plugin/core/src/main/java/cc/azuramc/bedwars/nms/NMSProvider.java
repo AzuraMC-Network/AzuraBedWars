39d5ebda33e1032d1ccec1bf909a6c8b26d6dccb
MindsMaster <1173348610@qq.com>
2025-10-11 02:38:59 +0800

    feat: Minecraft 1.21.10 support (experimental)

--- azurabedwars-plugin/core/src/main/java/cc/azuramc/bedwars/nms/NMSProvider.java
+++ azurabedwars-plugin/core/src/main/java/cc/azuramc/bedwars/nms/NMSProvider.java
@@ -13,14 +13,20 @@ import java.lang.reflect.InvocationTargetException;
 public class NMSProvider {
     private NMSAccess access;
     private String nmsVersion;
+    private boolean isExperimentalVersionSupport;
 
     public NMSAccess setup() {
+        if (VersionUtil.isGreaterOrEqual(1, 21, 10)) {
+            isExperimentalVersionSupport = true;
+        }
         this.nmsVersion = VersionUtil.getNmsVersion();
         this.access = createNMSAccess(nmsVersion);
 
         if (this.access == null) {
             LoggerUtil.warn("NMS支持未找到 (" + this.nmsVersion + ")! 启用兼容模式 可能存在意外问题!");
             access = new CompatibilityModeNMS();
+        } else if (isExperimentalVersionSupport) {
+            LoggerUtil.info("正在使用实验性版本支持 1.21.10 或更新的Minecraft版本");
         } else {
             LoggerUtil.info("正在使用受支持的版本! (" + this.nmsVersion + ")");
         }
@@ -29,7 +35,11 @@ public class NMSProvider {
     }
 
     private NMSAccess createNMSAccess(String version) {
+
         try {
+            if (isExperimentalVersionSupport) {
+                return (NMSAccess) Class.forName(this.getClass().getPackage().getName() + ".v1_21_10.NMS_v1_21_10").getDeclaredConstructor().newInstance();
+            }
             return (NMSAccess) Class.forName(this.getClass().getPackage().getName() + "." + version + ".NMS_" + version).getDeclaredConstructor().newInstance();
         } catch (ClassNotFoundException | NoSuchMethodException e) {
             LoggerUtil.warn("未受支持的版本: " + e.getMessage());
