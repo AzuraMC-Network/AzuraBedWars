781de2ef746bc668f3f886eb7d69a3f6ddcca360
MindsMaster <1173348610@qq.com>
2025-12-15 15:44:56 +0800

    fix: can't set world rule when use paper-1.21.11 (I don't know why they have diff bukkit api... also maybe some else reason)

--- azurabedwars-plugin/core/src/main/java/cc/azuramc/bedwars/compat/util/WorldUtil.java
+++ azurabedwars-plugin/core/src/main/java/cc/azuramc/bedwars/compat/util/WorldUtil.java
@@ -1,6 +1,7 @@
 package cc.azuramc.bedwars.compat.util;
 
 import cc.azuramc.bedwars.compat.VersionUtil;
+import cc.azuramc.bedwars.util.LoggerUtil;
 import cc.azuramc.bedwars.util.nms.ReflectionUtil;
 import org.bukkit.GameRule;
 import org.bukkit.World;
@@ -14,64 +15,64 @@ public class WorldUtil {
         world.setAutoSave(false);
 
         if (VersionUtil.isGreaterOrEqual(1, 21, 11)) {
-            world.setGameRule(GameRule.SPAWN_MOBS, false);
-            world.setGameRule(GameRule.FIRE_SPREAD_RADIUS_AROUND_PLAYER, 0);
-            world.setGameRule(GameRule.SHOW_ADVANCEMENT_MESSAGES, false);
-            return world;
+            if (trySetNewestGameRules(world)) {
+                return world;
+            }
         }
 
         if (VersionUtil.isGreaterOrEqual(1, 13)) {
-            // 使用 ReflectionUtil 调用 setGameRule 方法以兼容不同版本
-            try {
-                // 尝试获取 GameRule 枚举值
-                GameRule<?> doMobSpawning = getGameRule("DO_MOB_SPAWNING");
-                GameRule<?> doFireTick = getGameRule("DO_FIRE_TICK");
-                GameRule<?> announceAdvancements = getGameRule("ANNOUNCE_ADVANCEMENTS");
-
-                if (doMobSpawning != null) {
-                    ReflectionUtil.invokeMethod(world, "setGameRule", doMobSpawning, false);
-                }
-                if (doFireTick != null) {
-                    ReflectionUtil.invokeMethod(world, "setGameRule", doFireTick, false);
-                }
-                if (announceAdvancements != null) {
-                    ReflectionUtil.invokeMethod(world, "setGameRule", announceAdvancements, false);
-                }
-            } catch (Exception e) {
-                // 如果反射失败，回退到字符串方式
-                setGameRuleByString(world, "doMobSpawning", "false");
-                setGameRuleByString(world, "doFireTick", "false");
-                setGameRuleByString(world, "announceAdvancements", "false");
+            if (trySetLegacyGameRules(world)) {
+                return world;
             }
-            return world;
         }
 
-        // 1.13 以下版本使用字符串方式
-        setGameRuleByString(world, "doMobSpawning", "false");
-        setGameRuleByString(world, "doFireTick", "false");
-        setGameRuleByString(world, "announceAdvancements", "false");
+        trySetOldestGameRules(world);
         return world;
     }
 
-    /**
-     * 通过 ReflectionUtil 获取 GameRule 枚举值
-     */
-    private static GameRule<?> getGameRule(String name) {
+    private static boolean trySetNewestGameRules(World world) {
         try {
-            return ReflectionUtil.getStaticField(GameRule.class, name);
+            GameRule<?> spawnMobs = ReflectionUtil.getStaticField(GameRule.class, "SPAWN_MOBS");
+            GameRule<?> fireSpreadRadius = ReflectionUtil.getStaticField(GameRule.class, "FIRE_SPREAD_RADIUS_AROUND_PLAYER");
+            GameRule<?> showAdvancementMessages = ReflectionUtil.getStaticField(GameRule.class, "SHOW_ADVANCEMENT_MESSAGES");
+
+            ReflectionUtil.invokeMethod(world, "setGameRule", spawnMobs, false);
+            ReflectionUtil.invokeMethod(world, "setGameRule", fireSpreadRadius, 0);
+            ReflectionUtil.invokeMethod(world, "setGameRule", showAdvancementMessages, false);
+            LoggerUtil.debug("WorldUtil$trySetNewestGameRules | true");
+            return true;
         } catch (Exception e) {
-            return null;
+            LoggerUtil.debug("WorldUtil$trySetNewestGameRules | false");
+            return false;
         }
     }
 
-    /**
-     * 使用 ReflectionUtil 调用 setGameRuleValue 方法
-     */
-    private static void setGameRuleByString(World world, String rule, String value) {
+    private static boolean trySetLegacyGameRules(World world) {
         try {
-            ReflectionUtil.invokeMethod(world, "setGameRuleValue", rule, value);
+            GameRule<?> doMobSpawning = ReflectionUtil.getStaticField(GameRule.class, "DO_MOB_SPAWNING");
+            GameRule<?> doFireTick = ReflectionUtil.getStaticField(GameRule.class, "DO_FIRE_TICK");
+            GameRule<?> announceAdvancements = ReflectionUtil.getStaticField(GameRule.class, "ANNOUNCE_ADVANCEMENTS");
+
+            ReflectionUtil.invokeMethod(world, "setGameRule", doMobSpawning, false);
+            ReflectionUtil.invokeMethod(world, "setGameRule", doFireTick, false);
+            ReflectionUtil.invokeMethod(world, "setGameRule", announceAdvancements, false);
+            LoggerUtil.debug("WorldUtil$trySetLegacyGameRules | true");
+            return true;
         } catch (Exception e) {
-            // 静默失败，某些版本可能不支持
+            LoggerUtil.debug("WorldUtil$trySetLegacyGameRules | false");
+            return false;
+        }
+    }
+
+    private static void trySetOldestGameRules(World world) {
+        try {
+            ReflectionUtil.invokeMethod(world, "setGameRuleValue", "doMobSpawning", "false");
+            ReflectionUtil.invokeMethod(world, "setGameRuleValue", "doFireTick", "false");
+            ReflectionUtil.invokeMethod(world, "setGameRuleValue", "announceAdvancements", "false");
+            LoggerUtil.debug("WorldUtil$trySetOldestGameRules | true");
+        } catch (Exception e) {
+            LoggerUtil.error("Failed to set game rules for world: " + world.getName());
+            e.printStackTrace();
         }
     }
 }
